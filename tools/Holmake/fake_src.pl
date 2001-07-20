a0fbd060b933b448a038a0c4a59150a4a0aff3ec
Michael Norrish <Michael.Norrish@nicta.com.au>
2001-07-20 12:42:20 +0000

    Updated path to my hol directory.

--- tools/Holmake/fake_src.pl
+++ tools/Holmake/fake_src.pl
@@ -1,5 +1,5 @@
 while (<>) {
-  s|^val HOLDIR = .*$|val HOLDIR = "/local/scratch/mn200/Work/kan/"|;
+  s|^val HOLDIR = .*$|val HOLDIR = "/local/scratch/mn200/Work/hol98/"|;
   s|^val MOSMLDIR = .*$|val MOSMLDIR = "/local/scratch/kxs/200/"|;
   s|^val DEPDIR = .*$|val DEPDIR = ".HOLMK"|;
   s|^fun MK_XABLE file = .*$|fun MK_XABLE file = (Process.system ("chmod a+x "^file); file)|;
