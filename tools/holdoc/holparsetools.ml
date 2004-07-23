d9c51e8dadae802ef729dcb5e74acee485403764
Keith Wansbrough <keith@lochan.org>
2004-07-23 11:12:15 +0000

    Allow filename arguments to mng_to_latex, rather than accepting input from stdin only.  Thus report accurate filename:line:col locations in error messages, at last!

--- tools/holdoc/holparsetools.ml
+++ tools/holdoc/holparsetools.ml
@@ -17,11 +17,53 @@ let parse mode nonterminal lexbuf =
       raise (SyntaxError ("unexpected token "^Lexing.lexeme lexbuf^" at "^pretty_pos lexbuf))
 
 let parse_chan mode nonterminal chan =
-  let lexbuf = Lexing.from_channel stdin in
+  let lexbuf = Lexing.from_channel chan in
   parse mode nonterminal lexbuf
 
-(*
- * let parse_file mode nonterminal file =
- *   let lexbuf = Lexing.from_file file in
- *   parse mode nonterminal lexbuf
- *)  
+let move_to_start lexbuf file =
+  lexbuf.Lexing.lex_curr_p <- { Lexing.pos_fname = file;
+                                Lexing.pos_lnum = 1;
+                                Lexing.pos_bol = 0;
+                                Lexing.pos_cnum = 0;
+                              }
+
+let from_filelist files =
+  let lexbuf_r = ref None in
+  let files_r = ref files in
+  let chan_r = ref None in
+  let rec read buf len =
+    let Some lexbuf = !lexbuf_r in
+    match !chan_r with
+      Some chan ->
+        let n = input chan buf 0 len in
+        if n = 0 then begin
+          chan_r := None;
+          read buf len
+        end else
+          n
+    | None ->
+        match !files_r with
+          (file::files) ->
+            files_r := files;
+            chan_r := Some (open_in file);
+            move_to_start lexbuf file;
+            read buf len
+        | [] ->
+            move_to_start lexbuf "<eof>";
+            0
+  in
+  let lexbuf = Lexing.from_function read in
+  lexbuf_r := Some lexbuf;
+  lexbuf
+
+let parse_fileargs mode nonterminal =
+  let files = ref [] in
+  let handle_file f = files := f :: !files in
+  Arg.parse [] handle_file "Invalid argument";
+  let lexbuf =
+    if !files = [] then
+      Lexing.from_channel stdin  (* use stdin if no files listed *)
+    else
+      from_filelist (List.rev !files)
+  in
+  parse mode nonterminal lexbuf
--- tools/holdoc/holparsetools.mli
+++ tools/holdoc/holparsetools.mli
@@ -4,4 +4,7 @@ val parse :
   (('a -> Holparse.token) -> Lexing.lexbuf -> 'b) -> Lexing.lexbuf -> 'b
 val parse_chan :
   Holparsesupp.mode ->
-  (('a -> Holparse.token) -> Lexing.lexbuf -> 'b) -> 'c -> 'b
+  (('a -> Holparse.token) -> Lexing.lexbuf -> 'b) -> in_channel -> 'b
+val parse_fileargs :
+  Holparsesupp.mode ->
+  (('a -> Holparse.token) -> Lexing.lexbuf -> 'b) -> 'b
