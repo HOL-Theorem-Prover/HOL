0577c63476c268d9b199c59f59a429f6deadb59f
Konrad Slind <konrad.slind@gmail.com>
2005-04-12 13:40:56 +0000

    Upgraded hwDefine to handle decrement-based iterations on words. Now the factorials on word32 work.

--- examples/dev/Fact32/Fact32.ml
+++ examples/dev/Fact32/Fact32.ml
@@ -1,4 +1,3 @@
-
 (*****************************************************************************)
 (* High level (TFL) specification and implementation of factorial            *)
 (* using ``:word32`` instead of ``:num``                                     *)
@@ -7,83 +6,26 @@
 quietdec := true;
 loadPath := "../" :: "word32" :: "../dff/" :: !loadPath;
 map load
- ["compileTheory","compile","metisLib","intLib","word32Theory",
-  "dffTheory","vsynth","compile32Theory"];
+ ["compileTheory","compile","metisLib","intLib","word32Theory", "word32Lib",
+  "dffTheory","vsynth" (* ,"compile32Theory"*)];
 open compile metisLib word32Theory;
 open arithmeticTheory intLib pairLib pairTheory PairRules combinTheory
      devTheory composeTheory compileTheory compile vsynth dffTheory
-     compile32Theory;
+  (* compile32Theory *) ;
+quietdec := false;
+
 infixr 3 THENR;
 infixr 3 ORELSER;
 intLib.deprecate_int();
 
-(*****************************************************************************)
-(* The definition of hwDefine below is very temporary!                       *)
-(*****************************************************************************)
-fun CHEAT_TAC(asl,goal) = ([], fn _ => mk_thm(asl,goal));
+(*---------------------------------------------------------------------------*)
+(* Tell hwDefine about how to deal with "predecessor-based" recursions on    *)
+(* words. Allows factorial and other iterations to be dealt with.            *)
+(*---------------------------------------------------------------------------*)
 
-fun hwDefine defq =
- let val absyn0 = Parse.Absyn defq
- in 
-   case absyn0
-    of Absyn.APP(_,Absyn.APP(_,Absyn.IDENT(loc,"measuring"),def),f) =>
-        let val (deftm,names) = Defn.parse_defn def
-            val hdeqn = hd (boolSyntax.strip_conj deftm)
-            val (l,r) = boolSyntax.dest_eq hdeqn
-            val domty = pairSyntax.list_mk_prod 
-                          (map type_of (snd (boolSyntax.strip_comb l)))
-            val fty = Pretype.fromType (domty --> numSyntax.num)
-(*
-            val typedf = Parse.absyn_to_term 
-                             (Parse.term_grammar())
-                             (Absyn.TYPED(loc,f,fty))
-*)
-            val defn = Defn.mk_defn (hd names) deftm
-(*
-            val tac = EXISTS_TAC (mk_measure typedf)
-                       THEN TotalDefn.TC_SIMP_TAC [] default_simps
-*)
-            val tac = CHEAT_TAC
-            val (defth,ind) = Defn.tprove(defn, tac)
-            val (lt,rt) = boolSyntax.dest_eq(concl defth)
-            val (func,args) = dest_comb lt
-            val (test,t1,t2) = dest_cond rt
-            val fb = mk_pabs(args,test)
-            val f1 = mk_pabs(args,t1)
-            val f2 = mk_pabs(args,rand t2)
-(*
-            val totalth = prove
-                    (Term`TOTAL(^fb,^f1,^f2)`,
-                     RW_TAC std_ss [TOTAL_def,pairTheory.FORALL_PROD]
-                      THEN EXISTS_TAC typedf
-                      THEN TotalDefn.TC_SIMP_TAC [] default_simps)
-*)
-            val totalth = prove
-                    (Term`TOTAL(^fb,^f1,^f2)`,
-                     CHEAT_TAC)
-            val devth = PURE_REWRITE_RULE [GSYM DEV_IMP_def]
-                          (RecCompileConvert defth totalth)
-        in
-         (defth,ind,devth)
-        end
-     | otherwise => 
-        let val defth = SPEC_ALL(Define defq)
-            val _ =
-             if occurs_in 
-                 (fst(strip_comb(lhs(concl defth)))) 
-                 (rhs(concl defth))
-              then (print "definition of "; 
-                    print (fst(dest_const(fst(strip_comb(lhs(concl defth))))));
-                    print " is recusive; must have a measure";
-                    raise ERR "hwDefine" "recursive definition without a measure")
-              else ()
-            val devth = PURE_REWRITE_RULE[GSYM DEV_IMP_def] 
-                          (CompileConvert defth)
-        in (defth,boolTheory.TRUTH,devth)
-        end
- end;
-
-quietdec := false;
+val _ = 
+  compile.termination_simps := (word32Theory.WORD_PRED_THM :: 
+                                !compile.termination_simps);
 
 add_combinational ["MOD","WL","DIV"];
 add_combinational ["word_add","word_sub"];
@@ -101,6 +43,7 @@ add_temporal_abstractions
   SUB_at, 
   LESS_at];
 
+
 (*****************************************************************************)
 (* Start new theory "Fact32"                                                 *)
 (*****************************************************************************)
@@ -114,7 +57,7 @@ val (Mult32Iter,Mult32Iter_ind,Mult32Iter_dev) =
  hwDefine
   `(Mult32Iter (m,n,acc) =
      if m = 0w then (0w,n,acc) else Mult32Iter(m-1w,n,n + acc))
-   measuring FST`;
+   measuring (w2n o FST)`;
 
 (*****************************************************************************)
 (* Create an implementation of a multiplier from Mult32Iter                  *)
@@ -130,7 +73,7 @@ val (Fact32Iter,Fact32Iter_ind,Fact32Iter_dev) =
  hwDefine
   `(Fact32Iter (n,acc) =
      if n = 0w then (n,acc) else Fact32Iter(n-1w, Mult32(n,acc)))
-   measuring FST`;
+   measuring (w2n o FST)`;
 
 (*****************************************************************************)
 (* Implement a function Fact32 to compute SND(Fact32Iter (n,1))              *)
