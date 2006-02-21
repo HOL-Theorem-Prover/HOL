c2f84a8235a5be2c331187ccb32bc249bc01c73f
Mike Gordon <mjcg@cl.cam.ac.uk>
2006-02-21 16:16:52 +0000

    Update from Matt

--- examples/acl2/lisp/filter-forms.lisp
+++ examples/acl2/lisp/filter-forms.lisp
@@ -42,6 +42,71 @@
                                            new-body))
                   (otherwise (and (not basic-only) form))))))))
 
+(defconst *events-to-skip*
+  (cons
+   'local
+   (set-difference-eq
+    *primitive-event-macros*
+
+; Below is *primitive-event-macros*, but with those commented out that we know
+; we can skip.
+
+    '(
+      defun
+      #+:non-standard-analysis
+      defun-std
+      mutual-recursion
+      defuns ; maybe don't really care, though
+      defthm
+      #+:non-standard-analysis
+      defthm-std
+      defaxiom
+;     defconst ; ok to skip because it's translated away
+      defstobj
+      defpkg ; not actually in *primitive-event-macros*
+;     deflabel
+;     defdoc
+;     deftheory
+      defchoose
+;     verify-guards
+      defmacro
+;     in-theory
+;     in-arithmetic-theory
+;     push-untouchable
+;     table
+      encapsulate
+      include-book
+;     theory-invariant
+      verify-termination
+      logic
+      program
+;     add-default-hints!
+;     add-match-free-override
+      add-include-book-dir
+      delete-include-book-dir
+;     remove-default-hints!
+;     set-match-free-default
+;     set-enforce-redundancy
+;     set-verify-guards-eagerness
+;     set-non-linearp
+;     set-compile-fns
+;     set-measure-function
+;     set-well-founded-relation
+;     set-invisible-fns-table
+;     set-backchain-limit
+;     set-bogus-mutual-recursion-ok
+;     set-irrelevant-formals-ok
+;     set-ignore-ok
+;     set-inhibit-warnings
+;     set-state-ok
+;     set-let*-abstractionp
+;     set-nu-rewriter-mode
+;     set-case-split-limitations
+;     set-default-hints!
+;     set-rewrite-stack-limit
+;     comp
+      ))))
+
 (defun expand-forms (forms acc untrans-flg basic-only state)
 
 ; Result is in reverse order.
@@ -53,17 +118,29 @@
                (let ((form (car forms)))
                  (cond ((atom form) (value acc))
                        (t (case (car form)
-                            ((progn mutual-recursion)
-                             (er-let* ((defs (expand-forms (cdr form) nil
-                                                           untrans-flg
-                                                           basic-only
-                                                           state)))
-                                      (value
-                                       (if (null defs) ; expect basic-only
-                                           acc
-                                         (cons (cons (car form)
-                                                     (reverse defs))
-                                               acc)))))
+                            ((progn mutual-recursion encapsulate)
+                             (if (and (eq (car form) 'encapsulate)
+                                      (cadr form))
+                                 (value (cons form acc)) ; just punt
+                               (er-let* ((defs (expand-forms
+                                                (if (eq (car form)
+                                                        'encapsulate)
+                                                    (cddr form)
+                                                  (cdr form))
+                                                nil
+                                                untrans-flg
+                                                basic-only
+                                                state)))
+                                        (value
+                                         (cond
+                                          ((null defs) ; expect basic-only
+                                           acc)
+                                          ((eq (car form) 'encapsulate)
+                                           (append defs acc))
+                                          (t
+                                           (cons (cons (car form)
+                                                       (reverse defs))
+                                                 acc)))))))
                             ((defaxiom defthm defun)
                              (if (and basic-only
                                       (eq (car form) 'defun)
@@ -74,7 +151,10 @@
                                 ((x (expand-form form untrans-flg basic-only
                                                  state)))
                                 (value (cons x acc)))))
-                            (t (value (cons form acc)))))))))
+                            (t (if (member-eq (car form)
+                                              *events-to-skip*)
+                                   (value acc)
+                                 (value (cons form acc))))))))))
              (expand-forms (cdr forms) new-acc untrans-flg basic-only state))))
 
 (defun push-defuns-to-front (forms non-defuns defuns)
