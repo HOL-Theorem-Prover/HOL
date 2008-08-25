856609eba2d82acd9e5397315835efa0727f0014
Michael Norrish <Michael.Norrish@nicta.com.au>
2008-08-25 02:20:19 +0000

    Some Emacs key-bindings for Unicode characters of interest. <control>-! for universal quantification, for example.

---
+++ tools/hol-unicode.el
@@ -0,0 +1,15 @@
+(global-set-key (kbd "C-!") "∀")
+(global-set-key (kbd "C-?") "∃")
+(global-set-key (kbd "C-&") "∧")
+(global-set-key (kbd "C-|") "∨")
+(global-set-key (kbd "C-S-u") "∪")
+(global-set-key (kbd "C-S-i") "∩")
+(global-set-key (kbd "C-:") "∈")
+
+;; Greek : C-S-<char> for lower case version of Greek <char>
+;;         add the Meta modifier for upper case Greek letter.
+(global-set-key (kbd "C-S-l") "λ")
+(global-set-key (kbd "C-S-M-l") "Λ")
+(global-set-key (kbd "C-S-g") "γ")
+(global-set-key (kbd "C-S-M-g") "Γ")
+(global-set-key (kbd "C-S-M-d") "Δ")
