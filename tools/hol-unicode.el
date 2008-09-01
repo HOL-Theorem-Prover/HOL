bdf30ab76870905a5948c44834ea5f311e499ae8
Michael Norrish <Michael.Norrish@nicta.com.au>
2008-09-01 04:20:07 +0000

    More Greek letters, as well as bindings for subset and negation.  ('c' seems reminiscent of the subset shape, and isn't an obvious choice for any Greek letter I can think of.  Likewise 'u' and 'i' for union and intersection seem pretty safe.  (Who wants to write iotas anyway?))   For psi, phi and pi, I may set up C-S-p as a prefix binding, so that one would type <C-S-p i> to get pi, <C-S-p h> to get phi etc.  And C-S-M-p for the capital versions of these.

--- tools/hol-unicode.el
+++ tools/hol-unicode.el
@@ -5,11 +5,20 @@
 (global-set-key (kbd "C-S-u") "∪")
 (global-set-key (kbd "C-S-i") "∩")
 (global-set-key (kbd "C-:") "∈")
+(global-set-key (kbd "C-~") (lambda () (interactive) (insert "¬")))
+(global-set-key (kbd "C-S-c") "⊆")
 
 ;; Greek : C-S-<char> for lower case version of Greek <char>
 ;;         add the Meta modifier for upper case Greek letter.
-(global-set-key (kbd "C-S-l") "λ")
-(global-set-key (kbd "C-S-M-l") "Λ")
+(global-set-key (kbd "C-S-a") "α")
+(global-set-key (kbd "C-S-b") "β")
 (global-set-key (kbd "C-S-g") "γ")
+(global-set-key (kbd "C-S-d") "δ")
+(global-set-key (kbd "C-S-l") "λ")
+(global-set-key (kbd "C-S-m") "μ")
+(global-set-key (kbd "C-S-s") "σ")
+(global-set-key (kbd "C-S-t") "τ")
+
 (global-set-key (kbd "C-S-M-g") "Γ")
 (global-set-key (kbd "C-S-M-d") "Δ")
+(global-set-key (kbd "C-S-M-l") "Λ")
