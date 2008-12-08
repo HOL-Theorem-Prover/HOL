2073ba04653593f0db77896ba6865c6f6f707d92
Matt Kaufmann <kaufmann@cs.utexas.edu>
2008-12-08 23:14:22 +0000

    Missed in last check-in (leading '.' I guess)

---
+++ examples/acl2/.acl2holrc.bash
@@ -0,0 +1,10 @@
+# editable variables:
+
+export ACL2_SRC=/projects/acl2/v3-4-linux-64
+export ACL2=${ACL2_SRC}/openmcl-saved_acl2
+export ACL2_HOL=/u/kaufmann/projects/HOL/examples/acl2
+
+# end of editable variables
+
+export ACL2_SYSTEM_BOOKS=${ACL2_SRC}/books
+export ACL2_HOL_LISP=${ACL2_HOL}/lisp
