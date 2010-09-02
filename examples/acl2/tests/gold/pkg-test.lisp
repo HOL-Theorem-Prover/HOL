d4f0d75802f5fc829258acce157b064aebdf18b9
Matt Kaufmann <kaufmann@cs.utexas.edu>
2010-09-02 10:50:32 +0000

    Added tests for quoted symbols

--- examples/acl2/tests/gold/pkg-test.lisp
+++ examples/acl2/tests/gold/pkg-test.lisp
@@ -6,3 +6,55 @@
 (DEFUN MY-PKG::F1 (MY-PKG::X) (CONS MY-PKG::X MY-PKG::X))
 
 (DEFUN MY-PKG::F2 (MY-PKG::X) (BINARY-APPEND MY-PKG::X MY-PKG::X))
+
+(DEFUN MY-PKG::CONSTS NIL '(MY-PKG::X DEFUN CONS))
+
+(DEFTHM
+     TEST0
+     ((LAMBDA (LST)
+              (IF (EQUAL (CAR LST) 'MY-PKG::X)
+                  (IF (NOT (EQUAL (CAR LST) 'X))
+                      (IF (EQUAL (CAR (CDR LST)) 'DEFUN)
+                          (IF (EQUAL (CAR (CDR LST)) 'DEFUN)
+                              (IF (EQUAL (CAR (CDR LST)) 'DEFUN)
+                                  (IF (EQUAL (CAR (CDR (CDR LST))) 'CONS)
+                                      (IF (EQUAL (CAR (CDR (CDR LST))) 'CONS)
+                                          (EQUAL (CAR (CDR (CDR LST))) 'CONS)
+                                          'NIL)
+                                      'NIL)
+                                  'NIL)
+                              'NIL)
+                          'NIL)
+                      'NIL)
+                  'NIL))
+      (MY-PKG::CONSTS)))
+
+(DEFTHM TEST1A (EQUAL (SYMBOL-PACKAGE-NAME 'MY-PKG::C) '"MY-PKG"))
+
+(DEFTHM TEST1B (EQUAL (SYMBOL-NAME 'MY-PKG::C) '"C"))
+
+(DEFTHM TEST2A (EQUAL (SYMBOL-PACKAGE-NAME 'DEFUN) '"COMMON-LISP"))
+
+(DEFTHM TEST2B (EQUAL (SYMBOL-NAME 'DEFUN) '"DEFUN"))
+
+(DEFTHM TEST3A (EQUAL (SYMBOL-PACKAGE-NAME 'MY-PKG::|defun|) '"MY-PKG"))
+
+(DEFTHM TEST3B (EQUAL (SYMBOL-NAME 'MY-PKG::|defun|) '"defun"))
+
+(DEFTHM TEST4
+        (EQUAL (INTERN-IN-PACKAGE-OF-SYMBOL '"DEFUN"
+                                            'MY-PKG::C)
+               (INTERN-IN-PACKAGE-OF-SYMBOL '"DEFUN"
+                                            'COMMON-LISP::C)))
+
+(DEFTHM TEST5
+        (NOT (EQUAL (INTERN-IN-PACKAGE-OF-SYMBOL '"defun"
+                                                 'MY-PKG::C)
+                    (INTERN-IN-PACKAGE-OF-SYMBOL '"defun"
+                                                 'COMMON-LISP::C))))
+
+(DEFTHM TEST6
+        (NOT (EQUAL (INTERN-IN-PACKAGE-OF-SYMBOL '"D"
+                                                 'MY-PKG::C)
+                    (INTERN-IN-PACKAGE-OF-SYMBOL '"D"
+                                                 'COMMON-LISP::C))))
--- examples/acl2/tests/inputs/pkg-test.lisp
+++ examples/acl2/tests/inputs/pkg-test.lisp
@@ -1,3 +1,5 @@
+; (defpkg "MY-PKG" '(defun cons))
+
 (in-package "MY-PKG")
 
 (defun f1 (x)
@@ -5,3 +7,73 @@
 
 (defun f2 (x)
   (acl2::append x x))
+
+(defun consts ()
+  '(x defun cons))
+
+#!acl2
+(defthm test0
+  (let ((lst (my-pkg::consts)))
+    (and (equal (car lst) 'my-pkg::x)
+         (not (equal (car lst) 'acl2::x))
+         (equal (cadr lst) 'my-pkg::defun)
+         (equal (cadr lst) 'acl2::defun)
+         (equal (cadr lst) 'common-lisp::defun)
+         (equal (caddr lst) 'my-pkg::cons)
+         (equal (caddr lst) 'acl2::cons)
+         (equal (caddr lst) 'common-lisp::cons)))
+  :rule-classes nil)
+
+#!acl2
+(defthm test1a
+  (equal (symbol-package-name 'my-pkg::c)
+         "MY-PKG")
+  :rule-classes nil)
+
+#!acl2
+(defthm test1b
+  (equal (symbol-name 'my-pkg::c)
+         "C")
+  :rule-classes nil)
+
+#!acl2
+(defthm test2a
+  (equal (symbol-package-name 'my-pkg::defun)
+         "COMMON-LISP")
+  :rule-classes nil)
+
+#!acl2
+(defthm test2b
+  (equal (symbol-name 'my-pkg::defun)
+         "DEFUN")
+  :rule-classes nil)
+
+#!acl2
+(defthm test3a
+  (equal (symbol-package-name 'my-pkg::|defun|)
+         "MY-PKG")
+  :rule-classes nil)
+
+#!acl2
+(defthm test3b
+  (equal (symbol-name 'my-pkg::|defun|)
+         "defun")
+  :rule-classes nil)
+
+#!acl2
+(defthm test4
+  (equal (intern-in-package-of-symbol "DEFUN" 'my-pkg::c)
+         (intern-in-package-of-symbol "DEFUN" 'common-lisp::c))
+  :rule-classes nil)
+
+#!acl2
+(defthm test5
+  (not (equal (intern-in-package-of-symbol "defun" 'my-pkg::c)
+              (intern-in-package-of-symbol "defun" 'common-lisp::c)))
+  :rule-classes nil)
+
+#!acl2
+(defthm test6
+  (not (equal (intern-in-package-of-symbol "D" 'my-pkg::c)
+              (intern-in-package-of-symbol "D" 'common-lisp::c)))
+  :rule-classes nil)
