a6ec1c0aa2b19aa16ade11b9e28c9412a1391654
Ramana Kumar <ramana.kumar@gmail.com>
2011-09-17 11:48:41 +0100

    try importing base package into cl.thy

--- examples/ind_def/cl.thy
+++ examples/ind_def/cl.thy
@@ -1,11 +1,14 @@
 name: cl
-version: 0.1
+version: 0.2
 description: Combinatory logic example illustrating inductive definitions in HOL4
 author: Ramana Kumar <ramana.kumar@gmail.com>
 license: MIT
 show: "combinatoryLogicExample"
 show: "Data.Bool"
-
+base {
+  package: base-1.2
+}
 main {
+  import: base
   article: "cl.art"
 }
