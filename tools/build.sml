HOL-Theorem-Prover/HOL

commit 07b85603d1ace978cc74f410b9c06cabe0d2c004

Add -otknl option to bin/build for logging-kernel
Allow build-sequence paths to be filtered by which kernel is in use,
using (kernelname) akin to the existing [mlsys] filter.

Remove tools/logging-build-sequence.

This makes commits like 84c602e312 unnecessary as future changes are
made to the build sequence.

tools/build.sml
@@ -39,9 +39,6 @@ val {kernelspec,seqname = bseq_fname,rest = cmdline} =
       Normal x => x
     | Clean s => {kernelspec = "", seqname = dfltbuildseq, rest = [s]}
 
-(* use the experimental kernel? *)
-val use_expk = kernelspec = "-expk"
-
 (* do self-tests? and to what level *)
 val (do_selftests, cmdline) = cline_selftest cmdline
 
@@ -49,12 +46,11 @@ val (do_selftests, cmdline) = cline_selftest cmdline
      Source directories.
  ---------------------------------------------------------------------------*)
 
-val kpath = if use_expk then fullPath [HOLDIR, "src", "experimental-kernel"]
-            else fullPath [HOLDIR, "src", "0"]
+val kname = String.extract(kernelspec,1,NONE)
 
 val SRCDIRS =
     if cmdline = ["help"] then []
-    else read_buildsequence {kernelpath = kpath} bseq_fname
+    else read_buildsequence {kernelname = kname} bseq_fname
 
 val SIGOBJ = fullPath [HOLDIR, "sigobj"];
 val HOLMAKE = fullPath [HOLDIR, "bin/Holmake"]
@@ -399,6 +395,7 @@ fun clean_dirs f =
                           (fullPath [HOLDIR, "help", "src-sml"] ::
                            fullPath [HOLDIR, "src", "0"] ::
                            fullPath [HOLDIR, "src", "experimental-kernel"] ::
+                           fullPath [HOLDIR, "src", "logging-kernel"] ::
                            map #1 SRCDIRS)
 
 fun check_against s = let

tools-poly/build.sml
@@ -62,12 +62,14 @@ val (do_selftests, cmdline) = cline_selftest cmdline
      Source directories.
  ---------------------------------------------------------------------------*)
 
-val kpath = if use_expk then fullPath [HOLDIR, "src", "experimental-kernel"]
-            else fullPath [HOLDIR, "src", "0"]
+val kname =
+  if use_expk then "expk"
+  else if kernelspec = "-otknl" then "otknl"
+  else "stdknl"
 
 val SRCDIRS =
     if cmdline = ["help"] then []
-    else read_buildsequence {kernelpath = kpath} bseq_fname
+    else read_buildsequence {kernelname = kname} bseq_fname
 
 val SIGOBJ = fullPath [HOLDIR, "sigobj"];
 val HOLMAKE = fullPath [HOLDIR, "bin/Holmake"]

tools/build-sequence
@@ -8,8 +8,8 @@
 
 # The **KERNEL** line stands for the directory containing the sytem's
 # kernel, which may be selected as a command-line option to the build
-# command (the possibilities are src/0 or src/experimental-kernel as
-# of 17 May 2011).
+# command (the possibilities are src/0, src/experimental-kernel, or
+# src/logging-kernel as of 10 August 2012).
 
 # Comments can be added; all text on a line after a # symbol is
 # ignored.  Similarly blank lines are ignored.  If you are going to
@@ -32,8 +32,17 @@
 # name of the current ML implementation.  Thus the line [poly]foo
 # will cause foo to be built only if the current implementation is
 # Poly/ML.  (The other supported implementation at the time of writing
-# is [mosml].)  If combining this with exclamation marks, the [system]
-# annotation should come first, i.e., [mosml]!!foo, not !![mosml]foo.
+# is [mosml].)
+#
+# If a directory name is preceded by a string inside ()-brackets, then
+# that directory/target will only be considered if the string is the
+# name of the current kernel.  Thus the line (otknl)foo will cause foo to
+# be built only if the current kernel is otknl. (The other supported
+# kernels at the time of writing are stdknl and expk.)
+# 
+# If combining these annotations, the required order is [system] then
+# (kernel) then test level.  Thus [mosml]!!foo, not !![mosml]foo, and
+# [poly](otknl)!foo, not (otknl)[poly]!foo.
 
 
 tools/mlyacc/mlyacclib
@@ -136,6 +145,7 @@ src/complex
 src/HolQbf
 src/HolSmt
 src/opentheory
+(otknl)src/opentheory/logging
 !examples/misc
 # these examples require Muddy, which is MoscowML specific
 [mosml]!examples/muddy/muddyC

tools/buildutils.sig
@@ -9,7 +9,7 @@ sig
   val warn : string -> unit
 
   val read_buildsequence :
-      { kernelpath : string } ->
+      {kernelname : string} ->
       string -> (string * int) list
 
   val cline_selftest : string list -> (int * string list)

tools/buildutils.sml
@@ -53,6 +53,7 @@ in
   \   or: build cleanAll\n\
   \   or: build help.\n" ^ symlink_mesg ^
   "Add -expk to build an experimental kernel.\n\
+  \Add -otknl to build the OpenTheory proof logging kernel.\n\
   \Add -stdknl to force the standard kernel.\n\
   \Add -selftest to do self-tests, where defined.\n\
   \       Optionally follow -selftest with a number to indicate level\n\
@@ -63,7 +64,14 @@ in
 end
 
 
-fun read_buildsequence {kernelpath} bseq_fname = let
+fun read_buildsequence {kernelname} bseq_fname = let
+  val kernelpath = fullPath [HOLDIR, "src",
+    case kernelname of
+        "stdknl" => "0"
+      | "expk" => "experimental-kernel"
+      | "otknl" => "logging-kernel"
+      | _ => die ("Bad kernelname: "^kernelname)
+    ]
   val readline = TextIO.inputLine
   fun read_file acc fstr =
       case readline fstr of
@@ -84,20 +92,23 @@ fun read_buildsequence {kernelpath} bseq_fname = let
                   if String.sub(s,0) = #"!" then
                     extract_testcount (String.extract(s,1,NONE), acc+1)
                   else (s,acc)
-              fun extract_mlsys s =
-                  if String.sub(s,0) = #"[" then let
+              fun extract_brackets name l r s =
+                  if String.sub(s,0) = l then let
                       fun grabsys i =
-                          if String.sub(s,i) = #"]" then
+                          if String.sub(s,i) = r then
                             (String.substring(s,1,i-1),
                              String.extract(s,i+1,NONE))
                           else grabsys (i + 1)
                     in
                       grabsys 1
                       handle Subscript =>
-                             die ("Malformed system spec: "^s)
+                             die ("Malformed "^name^" spec: "^s)
                     end
                   else ("", s)
+              val extract_mlsys = extract_brackets "system" #"[" #"]"
+              val extract_kernel = extract_brackets "kernel" #"(" #")"
               val (mlsys,s) = extract_mlsys s
+              val (knl,s) = extract_kernel s
               val (dirname0,testcount) = extract_testcount (s,0)
               val dirname =
                   if dirname0 = "**KERNEL**" then kernelpath
@@ -105,7 +116,8 @@ fun read_buildsequence {kernelpath} bseq_fname = let
                   else fullPath [HOLDIR, dirname0]
               open FileSys
             in
-              if mlsys = "" orelse mlsys = Systeml.ML_SYSNAME then
+              if (mlsys = "" orelse mlsys = Systeml.ML_SYSNAME) andalso
+                 (knl = "" orelse knl = kernelname) then
                 if access (dirname, [A_READ, A_EXEC]) then
                   if isDir dirname orelse mlsys <> "" then
                     read_file ((dirname,testcount)::acc) fstr
@@ -231,7 +243,7 @@ datatype buildtype =
 exception QuickExit of buildtype
 fun get_cline {default_seq} = let
   val reader = TextIO.inputLine
-  (* handle -fullbuild vs -seq fname, and -expk vs -stdknl *)
+  (* handle -fullbuild vs -seq fname, and -expk vs -otknl vs -stdknl *)
   val oldopts = read_earlier_options reader
   val newopts = CommandLine.arguments()
   val _ =
@@ -257,18 +269,18 @@ fun get_cline {default_seq} = let
         end
       | (SOME f, new') => (f, new')
   val (knlspec, newopts) =
-      case inter ["-expk", "-stdknl"] newopts of
+      case inter ["-expk", "-otknl", "-stdknl"] newopts of
         [x] => (x, delete x newopts)
       | (result as (x::y::_)) =>
         (warn ("Specifying multiple kernel options; using "^x);
          (x, setdiff newopts result))
       | [] => let
         in
-          case inter ["-expk", "-stdknl"] oldopts of
+          case inter ["-expk", "-otknl", "-stdknl"] oldopts of
             [] => ("-stdknl", newopts)
           | [x] => (warn ("*** Using kernel option "^x^
                           " from earlier build command; \n\
-                          \    use -expk or -stdknl to override");
+                          \    use -expk, -otknl, or -stdknl to override");
                     (x, newopts))
           | x::y::_ =>
             (warn ("Cached build options specify multiple kernel options; \

tools/logging-build-sequence
@@ -1,185 +1,0 @@
-# This file specifies the sequence of directories that the build
-# program builds.  Each non-comment, non-blank line specifies a
-# directory which will be visited by the build program.  Directories
-# that are not absolute are interpreted with respect to the root of
-# the HOL directory. Build is special-cased so that some directories
-# are treated differently, but the default behaviour is that in each,
-# it will call Holmake --qof.
-
-# The **KERNEL** line stands for the directory containing the sytem's
-# kernel, which may be selected as a command-line option to the build
-# command (the possibilities are src/0 or src/experimental-kernel as
-# of 17 May 2011).
-
-# Comments can be added; all text on a line after a # symbol is
-# ignored.  Similarly blank lines are ignored.  If you are going to
-# edit this file, be aware that it does not record various
-# dependencies between the directories.  If dir1 is earlier than dir2,
-# then dir1 can not depend on dir2, but if dir1 is later than dir2,
-# then it may or may not depend on dir2.
-
-# If a directory name is preceded by an !, then it will only be built
-# if the -selftest flag is passed to Holmake, and the object files
-# that it builds will not be transferred to the sigobj directory.  If
-# there is more than one ! then the number of them indicates the level
-# of -selftest required in order to get that directory to be built at
-# all.  (If -selftest appears on its own, then that's level 1.  If you
-# write -selftest 2, that's level 2, which means directories with 2 !s
-# will get tested.)
-
-# If a directory name is preceded by a string inside []-brackets, then
-# that directory/target will only be considered if the string is the
-# name of the current ML implementation.  Thus the line [poly]foo
-# will cause foo to be built only if the current implementation is
-# Poly/ML.  (The other supported implementation at the time of writing
-# is [mosml].)  If combining this with exclamation marks, the [system]
-# annotation should come first, i.e., [mosml]!!foo, not !![mosml]foo.
-
-
-tools/mlyacc/mlyacclib
-[mosml]src/portableML/mosml
-[poly]src/portableML/poly
-src/portableML
-src/prekernel
-src/logging-kernel
-src/postkernel
-# these three directories are required to even run hol.bare.unquote
-src/parse
-src/bool
-src/1
-src/proofman
-[poly]bin/hol.bare
-
-src/compute/src
-src/HolSat/sat_solvers/minisat
-src/HolSat/sat_solvers/zc2hs
-src/HolSat
-src/taut
-src/marker
-src/q
-src/combin
-src/lite
-src/refute
-src/simp/src
-src/metis
-src/meson/src
-src/IndDef
-src/basicProof
-src/relation
-src/one
-src/pair/src
-src/sum
-src/tfl/src
-src/option
-src/num/theories
-src/num/reduce/src
-src/num/arith/src
-src/num
-src/num/termination
-
-# A Poly/ML heap, numheap, is deposited in src/num/termination to
-# speed up the last few theories (sets, extra numbers, and lists) before
-# bossLib
-src/num/extra_theories
-src/pred_set/src
-src/datatype/equiv
-src/datatype/record
-src/datatype
-!src/datatype/theory_tests
-src/list/src
-src/quantHeuristics
-src/unwind
-src/HolSat/vector_def_CNF
-src/boss
-!src/boss/theory_tests
-# Up to this point is needed for hol (which loads bossLib).
-# (Maybe unwind could be omitted).
-[poly]bin/hol
-
-!src/tfl/src/test
-src/TeX
-!examples/RSA
-src/sort
-src/string
-!src/string/theorytesting
-!examples/STE
-src/res_quan/src
-src/quotient/src
-!src/quotient/examples
-src/finite_map
-!examples/ind_def
-!!examples/decidable_separationLogic/src/
-src/bag
-!!examples/unification/triangular/first-order
-src/n-bit
-!examples/ARM/arm6-verification
-!!examples/ARM/arm6-verification/correctness
-src/ring/src
-src/integer
-!examples/rings
-src/llist
-src/path
-!src/integer/testing
-src/patricia
-src/update
-src/emit
-!!examples/ARM/v4
-!!examples/ARM/v7
-src/hol88
-!src/quotient/examples/lambda
-!src/quotient/examples/sigma
-src/rational
-src/real
-src/complex
-!!examples/separationLogic/src/
-src/HolQbf
-src/HolSmt
-src/opentheory
-src/opentheory/logging
-!examples/misc
-# these examples require Muddy, which is MoscowML specific
-[mosml]!examples/muddy/muddyC
-[mosml]!examples/muddy
-[mosml]!examples/HolBdd
-[mosml]!!examples/HolCheck
-src/datatype/inftree
-!examples/lambda/basics
-!examples/logic
-!!examples/unification/triangular/nominal
-!examples/lambda/barendregt
-!examples/computability
-!!examples/lambda/other-models
-!!examples/lambda/typing
-!!examples/computability/lambda
-!!examples/computability/register
-src/Boolify/src
-src/float
-src/probability
-src/temporal/src
-
-#
-# Examples only from here on
-#
-!examples/parity
-!examples/MLsyntax
-!examples/set-theory/zfset
-!examples/set-theory/vbg
-!examples/set-theory/hol_sets
-!!examples/category
-!!examples/miller/ho_prover
-!!examples/miller/miller
-!examples/ordinal
-
-# Crypto examples
-!!examples/Crypto/AES
-!!examples/Crypto/IDEA
-!!examples/Crypto/MARS
-!!examples/Crypto/RC6
-# the Crypto/Serpent/Reference example causes Poly/ML to crash
-[mosml]!!examples/Crypto/Serpent/Reference
-!!examples/Crypto/Serpent/Bitslice
-!!examples/Crypto/SHA-1
-!!examples/Crypto/TEA
-!!examples/Crypto/TWOFISH
-
-[poly]!!!examples/machine-code/lisp

commit 1fcac08199e253d81011abd8ac75539f5d0f51b9

Fix bug introduced by 07b85603, which broke bin/build cleanAll on mosml.

tools/build.sml
@@ -46,7 +46,11 @@ val (do_selftests, cmdline) = cline_selftest cmdline
      Source directories.
  ---------------------------------------------------------------------------*)
 
-val kname = String.extract(kernelspec,1,NONE)
+val kname =
+    case kernelspec of
+      "-expk" => "expk"
+    | "-otknl" => "otknl"
+    | _ => "stdknl"
 
 val SRCDIRS =
     if cmdline = ["help"] then []