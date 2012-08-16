HOL-Theorem-Prover/HOL

commit 44cef4623a8063819086c38ae8d138b610ee6d50

Improve Holmake's messages about recursive invocation.
Closes #64.

Also helps with #77 because the code controlling the recursive
invocations is now in Holmake_tools rather than in both versions of
Holmake.sml.

tools-poly/Holmake/Holmake.sml
@@ -441,7 +441,8 @@ end
 
 
 (* directory specific stuff here *)
-fun Holmake visiteddirs cline_additional_includes dir targets = let
+fun Holmake dirinfo visiteddirs cline_additional_includes targets = let
+  val {abspath=dir, relpath=dirnm} = dirinfo
   val _ = OS.FileSys.chDir dir
 
 
@@ -1324,42 +1325,14 @@ in
   if keep_going_flag then keep_going tgts else stop_on_failure tgts
 end
 
-fun maybe_recurse k = let
-  fun recurse visited (newdir,nm) =
-      if Binaryset.member(visited, newdir) then SOME visited
-      else let
-          val _ = warn ("Recursively calling Holmake in "^nm)
-        in
-          Holmake visited [] newdir []
-          before
-          OS.FileSys.chDir dir
-        end
-  fun do_em accg [] = if k() then SOME accg else NONE
-    | do_em accg (x::xs) = let
-      in
-        case recurse accg x of
-          SOME g' => do_em g' xs
-        | NONE => NONE
-      end
-  val visited = Binaryset.add(visiteddirs, dir)
-in
-  if no_prereqs then
-    if k() then SOME visited else NONE
-  else let
-      fun foldthis (dir, m) =
-          Binarymap.insert(m, OS.FileSys.fullPath dir, dir)
-          handle OS.SysErr _ =>
-                 (warn ("Includes path "^dir^" looks bogus");
-                  m)
-      val possible_calls = List.foldr foldthis
-                                      (Binarymap.mkDict String.compare)
-                                      (cline_additional_includes @
-                                       envlist "PRE_INCLUDES" @
-                                       hmake_includes)
-    in
-      do_em visited (Binarymap.listItems possible_calls)
-    end
-end
+fun hm_recur k =
+    maybe_recurse
+        {warn = warn, no_prereqs = no_prereqs, hm = Holmake,
+         visited = visiteddirs,
+         includes =
+         cline_additional_includes @ envlist "PRE_INCLUDES" @ hmake_includes,
+         dir = {abspath = dir, relpath = dirnm},
+         local_build = k}
 
 in
   case targets of
@@ -1376,7 +1349,7 @@ in
           end
         else ()
     in
-      maybe_recurse (fn () => finish_logging (strategy  targets))
+      hm_recur (fn () => finish_logging (strategy  targets))
     end
   | xs => let
       fun isPhony x = member x ["clean", "cleanDeps", "cleanAll"] orelse
@@ -1385,7 +1358,7 @@ in
       if List.all isPhony xs then
         if finish_logging (strategy xs) then SOME visiteddirs else NONE
       else
-        maybe_recurse (fn () => finish_logging (strategy xs))
+        hm_recur (fn () => finish_logging (strategy xs))
     end
 end
 
@@ -1424,9 +1397,10 @@ in
   else let
       open OS.Process
       val result =
-          Holmake (Binaryset.empty String.compare)
+          Holmake {relpath = OS.Path.currentArc,
+                   abspath = OS.FileSys.getDir()}
+                  (Binaryset.empty String.compare)
                   cline_additional_includes
-                  (OS.FileSys.getDir())
                   targets
                   handle Fail s => (print ("Fail exception: "^s^"\n");
                                     exit failure)

tools/Holmake/Holmake.sml
@@ -449,7 +449,8 @@ end
 
 
 (* directory specific stuff here *)
-fun Holmake visiteddirs cline_additional_includes dir targets = let
+fun Holmake dirinfo visiteddirs cline_additional_includes targets = let
+  val {abspath=dir,relpath=dirnm} = dirinfo
   val _ = OS.FileSys.chDir dir
 
 
@@ -1179,43 +1180,14 @@ in
   if keep_going_flag then keep_going tgts else stop_on_failure tgts
 end
 
-fun maybe_recurse k = let
-  fun recurse visited (newdir,nm) =
-      if Binaryset.member(visited, newdir) then SOME visited
-      else let
-          val _ = warn ("Recursively calling Holmake in "^nm)
-        in
-          Holmake visited [] newdir []
-          before
-          FileSys.chDir dir
-        end
-  fun do_em accg [] = if k() then SOME accg else NONE
-    | do_em accg (x::xs) = let
-      in
-        case recurse accg x of
-          SOME g' => do_em g' xs
-        | NONE => NONE
-      end
-  val visited = Binaryset.add(visiteddirs, dir)
-in
-  if no_prereqs then
-    if k() then SOME visited else NONE
-  else let
-      fun foldthis (dir, m) =
-          Binarymap.insert(m, FileSys.fullPath dir, dir)
-          handle OS.SysErr _ =>
-                 (warn ("Includes path "^dir^" looks bogus");
-                  m)
-      val possible_calls = List.foldr foldthis
-                                      (Binarymap.mkDict String.compare)
-                                      (cline_additional_includes @
-                                       envlist "PRE_INCLUDES" @
-                                       hmake_includes)
-    in
-      do_em visited (Binarymap.listItems possible_calls)
-    end
-end
-
+fun hm_recur k =
+    maybe_recurse
+        {warn = warn, no_prereqs = no_prereqs, hm = Holmake,
+         visited = visiteddirs,
+         includes =
+         cline_additional_includes @ envlist "PRE_INCLUDES" @ hmake_includes,
+         dir = {abspath = dir, relpath = dirnm},
+         local_build = k}
 in
   case targets of
     [] => let
@@ -1225,7 +1197,7 @@ in
         print("Generated targets are: "^print_list (map fromFile targets)^"\n")
         else ()
     in
-      maybe_recurse
+      hm_recur
           (fn () => finish_logging (strategy  (map (fromFile) targets)))
     end
   | xs => let
@@ -1234,7 +1206,7 @@ in
     in
       if List.all isPhony xs then
         if finish_logging (strategy xs) then SOME visiteddirs else NONE
-      else maybe_recurse (fn () => finish_logging (strategy xs))
+      else hm_recur (fn () => finish_logging (strategy xs))
     end
 end
 
@@ -1271,9 +1243,10 @@ val _ =
   else let
       open Process
       val result =
-          Holmake (Binaryset.empty String.compare)
+          Holmake {relpath = OS.Path.currentArc,
+                   abspath = OS.FileSys.getDir()}
+                  (Binaryset.empty String.compare)
                   cline_additional_includes
-                  (OS.FileSys.getDir())
                   targets
                   handle Fail s => (print ("Fail exception: "^s^"\n");
                                     exit failure)

tools/Holmake/Holmake_tools.sig
@@ -36,5 +36,15 @@ sig
   val clean_dir : {extra_cleans: string list} -> unit
   val clean_depdir : {depdirname : string} -> bool
 
-end
+  val maybe_recurse :
+      {warn: string -> unit,
+       no_prereqs : bool,
+       hm: {relpath:string,abspath:string} -> string Binaryset.set ->
+           string list -> string list -> string Binaryset.set option,
+       visited: string Binaryset.set,
+       includes : string list,
+       dir : {abspath: string, relpath: string},
+       local_build : unit -> bool} ->
+      string Binaryset.set option
 
+end

tools/Holmake/Holmake_tools.sml
@@ -221,4 +221,45 @@ end handle OS.SysErr (mesg, _) => let
            end
          | DirNotFound => true
 
+
+fun maybe_recurse {warn,no_prereqs,hm,visited,includes,dir,local_build=k} =
+let
+  val {abspath=dir,relpath} = dir
+  fun recurse visited (newdir,nm) =
+      if Binaryset.member(visited, newdir) then SOME visited
+      else let
+          val nm = Path.mkCanonical (Path.concat(relpath, nm))
+          val _ = warn ("Recursively calling Holmake in "^nm)
+        in
+          hm {relpath=nm,abspath=newdir} visited [] []
+          before
+          (warn ("Finished recursive invocation in "^nm);
+           FileSys.chDir dir)
+        end
+  fun do_em accg [] = if k() then SOME accg else NONE
+    | do_em accg (x::xs) = let
+      in
+        case recurse accg x of
+          SOME g' => do_em g' xs
+        | NONE => NONE
+      end
+  val visited = Binaryset.add(visited, dir)
+in
+  if no_prereqs then
+    if k() then SOME visited else NONE
+  else let
+      fun foldthis (dir, m) =
+          Binarymap.insert(m, FileSys.fullPath dir, dir)
+          handle OS.SysErr _ =>
+                 (warn ("Includes path "^dir^" looks bogus");
+                  m)
+      val possible_calls = List.foldr foldthis
+                                      (Binarymap.mkDict String.compare)
+                                      includes
+
+    in
+      do_em visited (Binarymap.listItems possible_calls)
+    end
+end
+
 end (* struct *)

commit f5f11785b2b329f27abd925a5216218a8c13be22

Get recursive Holmake to handle absolute paths in INCLUDES.
This was broken by 44cef4623, handling #64.

tools-poly/Holmake/Holmake.sml
@@ -1397,7 +1397,7 @@ in
   else let
       open OS.Process
       val result =
-          Holmake {relpath = OS.Path.currentArc,
+          Holmake {relpath = SOME (OS.Path.currentArc),
                    abspath = OS.FileSys.getDir()}
                   (Binaryset.empty String.compare)
                   cline_additional_includes

tools/Holmake/Holmake.sml
@@ -1243,7 +1243,7 @@ val _ =
   else let
       open Process
       val result =
-          Holmake {relpath = OS.Path.currentArc,
+          Holmake {relpath = SOME (OS.Path.currentArc),
                    abspath = OS.FileSys.getDir()}
                   (Binaryset.empty String.compare)
                   cline_additional_includes

tools/Holmake/Holmake_tools.sig
@@ -39,11 +39,11 @@ sig
   val maybe_recurse :
       {warn: string -> unit,
        no_prereqs : bool,
-       hm: {relpath:string,abspath:string} -> string Binaryset.set ->
+       hm: {relpath:string option,abspath:string} -> string Binaryset.set ->
            string list -> string list -> string Binaryset.set option,
        visited: string Binaryset.set,
        includes : string list,
-       dir : {abspath: string, relpath: string},
+       dir : {abspath: string, relpath: string option},
        local_build : unit -> bool} ->
       string Binaryset.set option
 

tools/Holmake/Holmake_tools.sml
@@ -228,10 +228,16 @@ let
   fun recurse visited (newdir,nm) =
       if Binaryset.member(visited, newdir) then SOME visited
       else let
-          val nm = Path.mkCanonical (Path.concat(relpath, nm))
+          val newrelpath =
+              if Path.isAbsolute nm then NONE
+              else
+                case relpath of
+                  NONE => NONE
+                | SOME d => SOME (Path.mkCanonical (Path.concat(d, nm)))
+          val nm = case newrelpath of NONE => newdir | SOME d => d
           val _ = warn ("Recursively calling Holmake in "^nm)
         in
-          hm {relpath=nm,abspath=newdir} visited [] []
+          hm {relpath=newrelpath,abspath=newdir} visited [] []
           before
           (warn ("Finished recursive invocation in "^nm);
            FileSys.chDir dir)