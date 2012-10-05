HOL-Theorem-Prover/HOL

commit 7d7a664cab16dbba7e975c682a5840d318d01d40

Make Systeml.sig the same in Moscow ML and Poly builds.
This additional uniformity allows me to delete the
tools-poly/Holmake/Systeml.sig file.  This then requires a bunch of
changes to compilation of the early tools which have to point at
tools/Holmake/Systeml.sig rather than the version in
tools-poly/Holmake.

This should fix the configuration errors on MoscowML introduced by
0a0988fc.

tools/Holmake/winNT-systeml.sml
@@ -79,16 +79,21 @@ fun fullPath slist =
   end
 
 
-val HOLDIR =
-val MOSMLDIR =
-val HAVE_BASIS2002 =
-val OS =
-val DEPDIR =
-val GNUMAKE =
-val DYNLIB =
-val version =
-val ML_SYSNAME =
-val release =
+val HOLDIR = ""
+val MOSMLDIR = ""
+val HAVE_BASIS2002 = ""
+val OS = ""
+val POLY = ""
+val POLYMLLIBDIR = ""
+val POLY_LDFLAGS = []
+val POLY_LDFLAGS_STATIC = []
+val CC = ""
+val DEPDIR = ""
+val GNUMAKE = ""
+val DYNLIB = ""
+val version = ""
+val ML_SYSNAME = ""
+val release = ""
 
 val isUnix = false
 

tools-poly/Holmake/Systeml.sig
@@ -1,43 +1,0 @@
-signature Systeml =
-sig
-
-  val systeml : string list -> OS.Process.status
-  val system_ps : string -> OS.Process.status
-  val exec : string * string list -> 'a
-  val protect : string -> string
-  val xable_string : string -> string
-  val mk_xable : string -> string
-
-(* first argument to these are the name of the desired executable, the
-   second is the name of the post-initialisation script to run. *)
-  val emit_hol_script : string -> string -> string list -> string
-  val emit_hol_unquote_script : string -> string -> string list -> string
-
-  val find_my_path : unit -> string
-
-  (* configuration time constants *)
-  val HOLDIR : string
-  val POLYMLLIBDIR : string
-  val POLY : string
-  val POLY_LDFLAGS : string list
-  val POLY_LDFLAGS_STATIC : string list
-  val CC : string
-  val OS : string
-  val DEPDIR : string
-  val GNUMAKE : string
-  val DYNLIB : bool
-  val ML_SYSNAME : string
-
-  val isUnix : bool
-
-  (* other system-wide constants, shared between build tools and the
-     running hol *)
-  val build_log_dir : string
-  val build_log_file : string
-  val make_log_file : string
-
-  (* canonical source of version information *)
-  val release : string
-  val version : int
-
-end;

tools-poly/Holmake/poly-Holmake.ML
@@ -13,8 +13,8 @@ fun useC f =
     (use ("../tools/Holmake/" ^ f ^ ".sig");
      use ("../tools/Holmake/" ^ f ^ ".sml"))
 
-
-val _ = useH "Systeml";
+val _ = use "../tools/Holmake/Systeml.sig"
+val _ = use "Holmake/Systeml.sml";
 val _ = useC "internal_functions";
 val _ = useC "Holmake_types";
 val _ = useC "Holmake_tools";

tools-poly/Holmake/unix-systeml.sml
@@ -83,20 +83,22 @@ fun fullPath slist =
 
 
 (* these values are filled in by configure.sml *)
-val HOLDIR =
-val POLYMLLIBDIR =
-val POLY =
-val POLY_LDFLAGS =
-val POLY_LDFLAGS_STATIC =
-val CC =
-val OS =
+val HOLDIR = ""
+val MOSMLDIR = ""
+val HAVE_BASIS2002 = false
+val POLYMLLIBDIR = ""
+val POLY = ""
+val POLY_LDFLAGS = []
+val POLY_LDFLAGS_STATIC = []
+val CC = ""
+val OS = ""
 
-val DEPDIR =
-val GNUMAKE =
-val DYNLIB =
-val version =
-val ML_SYSNAME =
-val release =
+val DEPDIR = ""
+val GNUMAKE = ""
+val DYNLIB = ""
+val version = ""
+val ML_SYSNAME = ""
+val release = ""
 
 val isUnix = true
 

tools-poly/build.sml
@@ -486,12 +486,12 @@ in
      warn ("WARNING! WARNING!");
      ignore (TextIO.inputLine TextIO.stdIn))
   else ()
-end;
+end handle OS.SysErr _ => die ("File "^s^" has disappeared.");
 
 val _ = check_against "tools-poly/smart-configure.sml"
 val _ = check_against "tools-poly/configure.sml"
 val _ = check_against "tools-poly/build.sml"
-val _ = check_against "tools-poly/Holmake/Systeml.sig"
+val _ = check_against "tools/Holmake/Systeml.sig"
 
 fun symlink_check() =
     if OS = "winNT" then

tools-poly/buildheap.ML
@@ -9,7 +9,7 @@ fun die s = (warn s; OS.Process.exit OS.Process.failure)
 
 fun fullPath ps = List.foldl (fn (p,acc) => OS.Path.concat(acc,p))
                              (hd ps) (tl ps);
-use "Holmake/Systeml.sig";
+use "../tools/Holmake/Systeml.sig";
 use "Holmake/Systeml.sml";
 open Systeml;
 

tools-poly/configure.sml
@@ -186,7 +186,7 @@ val _ = let
   (* copy system-specific implementation of Systeml into place *)
   val srcfile = fullPath [holmakedir, OSkind ^"-systeml.sml"]
   val destfile = fullPath [holmakedir, "Systeml.sml"]
-  val sigfile = fullPath [holmakedir, "Systeml.sig"]
+  val sigfile = fullPath [holdir, "tools", "Holmake", "Systeml.sig"]
 in
   print "\nLoading system specific functions\n";
   use sigfile;
@@ -233,32 +233,24 @@ open Systeml;
 
 let
   val _ = print "Compiling system specific functions ("
-  (*val modTime = FileSys.modTime*)
-  val dir_0 = FileSys.getDir()
-  val sigfile = fullPath [holmakedir, "Systeml.sig"]
-  val sigfile_newer = true
-  val uifile = fullPath [holmakedir, "Systeml.ui"]
-  fun die () = (print ")\nFailed to compile system-specific code\n";
-                Process.exit Process.failure)
-  val systeml = fn l => if not (Process.isSuccess (systeml l)) then die() else ()
-  fun to_sigobj s = bincopy s (fullPath [holdir, "sigobj", s])
+  val sigfile = fullPath [holdir, "tools", "Holmake", "Systeml.sig"]
+  val uifile = fullPath [holdir, "sigobj", "Systeml.ui"]
+  fun to_sigobj s = bincopy s (fullPath [holdir, "sigobj", Path.file s])
 in
-  FileSys.chDir holmakedir;
   let val oo = TextIO.openOut uifile
   in
     TextIO.output (oo, fullPath [holdir, "sigobj", "Systeml.sig"] ^ "\n");
     TextIO.closeOut oo
   end;
-  app to_sigobj ["Systeml.sig", "Systeml.ui"];
+  to_sigobj sigfile;
   print "sig ";
-  let val oo = TextIO.openOut (fullPath [holmakedir, "Systeml.uo"])
+  let val oo = TextIO.openOut (fullPath [holdir, "sigobj", "Systeml.uo"])
   in
     TextIO.output (oo, fullPath [holdir, "sigobj", "Systeml.sml"] ^ "\n");
     TextIO.closeOut oo
   end;
-  app to_sigobj ["Systeml.sml", "Systeml.uo"];
-  print "sml)\n";
-  FileSys.chDir dir_0
+  to_sigobj (fullPath [holmakedir, "Systeml.sml"]);
+  print "sml)\n"
 end;
 
 

tools-poly/heapname.ML
@@ -2,7 +2,7 @@
 val _ = PolyML.Compiler.prompt1:="";
 val _ = PolyML.Compiler.prompt2:="";
 val _ = PolyML.print_depth 0;
-val _ = use "Holmake/Systeml.sig";
+val _ = use "../tools/Holmake/Systeml.sig";
 val _ = use "Holmake/Systeml.sml";
 val _ = use "poly/Binarymap.sig";
 val _ = use "poly/Binarymap.sml";

commit 7a9bad7a75782da47e27b4d15aca2343f3d9a281

Fix winNT-systeml.sml to compile again (broken by 7d7a664c)

tools/Holmake/winNT-systeml.sml
@@ -106,7 +106,7 @@ local
   fun munge s = String.translate (fn #"/" => "\\" | c => str c) s
   fun q s = "\""^munge s^"\""
 in
-fun emit_hol_script target qend =
+fun emit_hol_script target qend _ =
  let val ostrm = fopen(target^".bat")
      fun output s = TextIO.output(ostrm, s)
      val sigobj = q (fullPath [HOLDIR, "sigobj"])
@@ -123,7 +123,7 @@ fun emit_hol_script target qend =
  end
 
 
-fun emit_hol_unquote_script target qend =
+fun emit_hol_unquote_script target qend _ =
  let val ostrm = fopen(target^".bat")
      fun output s = TextIO.output(ostrm, s)
      val qfilter = q (fullPath [HOLDIR, "bin", "unquote"])
@@ -146,5 +146,3 @@ end (* local *)
 
 
 end; (* struct *)
-
-