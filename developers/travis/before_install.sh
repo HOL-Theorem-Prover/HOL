5feee0b6c40b291b669d563b74a4ea7bd47b189d
Michael Norrish <michael.norrish@nicta.com.au>
2013-11-18 10:16:36 +1100

    Shouldn't have unquoted ampersands on command-lines.
    And of course, ampersands appear all over the place in URLs...

--- developers/travis/before_install.sh
+++ developers/travis/before_install.sh
@@ -4,7 +4,8 @@ set -e
 
 cd
 
-wget -q -O polyml5.5.1.tar.gz http://downloads.sourceforge.net/project/polyml/polyml/5.5.1/polyml.5.5.1.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpolyml%2Ffiles%2Fpolyml%2F5.5.1%2Fpolyml.5.5.1.tar.gz%2Fdownload&ts=1384728510&use_mirror=softlayer-dal
+wget -O polyml5.5.1.tar.gz "http://downloads.sourceforge.net/project/polyml/polyml/5.5.1/polyml.5.5.1.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpolyml%2Ffiles%2Fpolyml%2F5.5.1%2Fpolyml.5.5.1.tar.gz%2Fdownload&ts=1384728510&use_mirror=softlayer-dal"
+
 tar xvzf polyml.5.5.1.tar.gz
 cd polyml.5.5.1
 ./configure --prefix=$HOME
