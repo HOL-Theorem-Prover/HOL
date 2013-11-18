49a0cd9585516e31c04937966d04667f50a3f5e3
Michael Norrish <michael.norrish@nicta.com.au>
2013-11-18 12:04:29 +1100

    Travis: another attempt to build Poly in both user- and root-land.

--- developers/travis/before_install.sh
+++ developers/travis/before_install.sh
@@ -6,14 +6,16 @@ cd
 
 wget -q -O polyml5.5.1.tar.gz "http://downloads.sourceforge.net/project/polyml/polyml/5.5.1/polyml.5.5.1.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpolyml%2Ffiles%2Fpolyml%2F5.5.1%2Fpolyml.5.5.1.tar.gz%2Fdownload&ts=1384728510&use_mirror=softlayer-dal"
 
-tar xvzf polyml5.5.1.tar.gz
+tar xzf polyml5.5.1.tar.gz
 cd polyml.5.5.1
-if [ -n "$LD_LIBRARY_PATH" ]
+if [ -z "$ROOTPOLY" ]
 then
+  echo "*** Installing PolyML in home directory"
   ./configure --prefix=$HOME --enable-shared
   make
   make install
 else
+  echo "*** Installing PolyML in root land directory"
   ./configure --enable-shared
   make
   sudo make install
