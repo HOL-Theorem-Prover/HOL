HOL-Theorem-Prover/HOL

commit 1032a64e12aa66040de73e6eb09be93900a7850e

Fiddle with datatype printing in TeX (with selftest of sorts)

src/TeX/EmitTeX.sml
@@ -408,16 +408,17 @@ let val {add_string,add_break,begin_block,add_newline,end_block,...} =
         let val l = strip_type (type_of t)
             val ll = length l
         in
-          (PT t;
-             if ll < 2 then
-               ()
-             else
-               (S " "; S "of"; BR(1,0);
-                BB PP.INCONSISTENT 0;
-                  app (fn x => (TP x; S " "; S "=>"; BR(1,0)))
-                      (List.take(l, ll - 2));
-                  TP (List.nth(l, ll - 2));
-                EB()))
+          BB PP.CONSISTENT 0;
+          PT t;
+          if ll < 2 then ()
+          else
+            (S " "; S "of"; BR(1,2);
+             BB PP.INCONSISTENT 0;
+             app (fn x => (TP x; S " "; S "=>"; BR(1,0)))
+                 (List.take(l, ll - 2));
+             TP (List.nth(l, ll - 2));
+             EB());
+          EB()
         end
 
     fun enumerated_type n =
@@ -434,12 +435,23 @@ let val {add_string,add_break,begin_block,add_newline,end_block,...} =
       end
 
     fun pp_constructor_spec (n, l) =
-          (if !print_datatype_names_as_types then pp_type_name n else PT n; BR(1,2);
-           BB (if enumerated_type n then PP.INCONSISTENT else PP.CONSISTENT) 0;
-             S "= ";
-             app (fn x => (pp_clause x; BR(1,0); S "|"; S " "))
-                 (List.take(l, length l - 1));
-             pp_clause (last l);
+        if !print_datatype_names_as_types then pp_type_name n
+        else if enumerated_type n then
+          (BB PP.CONSISTENT 0;
+           PT n; S " ";
+           BB PP.INCONSISTENT 0;
+           S "="; S " ";
+           app (fn x => (pp_clause x; BR(1,0); S"|"; S" "))
+               (List.take(l, length l - 1));
+           pp_clause(last l);
+           EB();
+           EB())
+        else
+          (BB PP.CONSISTENT 2;
+           PT n; S " "; S "="; BR(1,2);
+           app (fn x => (pp_clause x; BR(1,0); S "|"; S " "))
+               (List.take(l, length l - 1));
+           pp_clause (last l);
            EB())
 
     fun pp_record_spec l =
@@ -448,11 +460,11 @@ let val {add_string,add_break,begin_block,add_newline,end_block,...} =
         in
           (PT (hd l); S " ="; BR(1,2);
            BB PP.CONSISTENT 3;
-             S "<| ";
-             app (fn x => (pp_record x; S ";"; BR(1,0)))
+           S "<|"; S " ";
+           app (fn x => (pp_record x; S ";"; BR(1,0)))
                (List.take(ll, length ll - 1));
-             pp_record (last ll);
-             S " |>";
+           pp_record (last ll);
+           S " "; S "|>";
            EB())
         end
 

src/TeX/selftest.sml
@@ -63,3 +63,29 @@ val _ = tprint "Testing UNIV printing (:num)"
 val s = pp_to_string 70 pp_term_as_tex (pred_setSyntax.mk_univ numSyntax.num)
 val _ = if s = "\\ensuremath{\\cal{U}}(:\\HOLTyOp{num})" then print "OK\n"
         else die()
+
+val _ = Feedback.emit_MESG := false
+fun dtype_test n s = pp_to_string n (raw_pp_theorem_as_tex (fn _ => NONE)) (theorem ("datatype_" ^ s))
+val _ = tprint "Testing datatype printing"
+val _ = Hol_datatype`foo = C of bool -> 'a -> bool | D of foo => 'a => num list | Econ of foo => foo => 'a`;
+val s = dtype_test 55 "foo"
+val _ = if s = "\\HOLFreeVar{foo} =\n\
+               \    \\HOLConst{C} \\HOLKeyword{of} \\HOLTyOp{bool} -> \\ensuremath{\\alpha} -> \\HOLTyOp{bool}\n\
+               \  \\HOLTokenBar{} \\HOLConst{D} \\HOLKeyword{of} \\ensuremath{\\alpha} \\HOLTyOp{foo} \\HOLTokenImp{} \\ensuremath{\\alpha} \\HOLTokenImp{} \\HOLTyOp{num} \\HOLTyOp{list}\n\
+               \  \\HOLTokenBar{} \\HOLConst{Econ} \\HOLKeyword{of} \\ensuremath{\\alpha} \\HOLTyOp{foo} \\HOLTokenImp{} \\ensuremath{\\alpha} \\HOLTyOp{foo} \\HOLTokenImp{} \\ensuremath{\\alpha}"
+        then
+          print "OK\n"
+        else die()
+
+val _ = tprint "Testing enumerated datatype printing"
+val _ = Hol_datatype `bar = ETA | ETB | ETC | ETD | ETE | ETF | ETG | ETH | ETI | ETJ`;
+val s = dtype_test 20 "bar"
+val _ = if s = "\\HOLFreeVar{bar} = \\HOLConst{ETA} \\HOLTokenBar{} \\HOLConst{ETB}\n\
+               \    \\HOLTokenBar{} \\HOLConst{ETC} \\HOLTokenBar{} \\HOLConst{ETD}\n\
+               \    \\HOLTokenBar{} \\HOLConst{ETE} \\HOLTokenBar{} \\HOLConst{ETF}\n\
+               \    \\HOLTokenBar{} \\HOLConst{ETG} \\HOLTokenBar{} \\HOLConst{ETH}\n\
+               \    \\HOLTokenBar{} \\HOLConst{ETI} \\HOLTokenBar{} \\HOLConst{ETJ}"
+        then
+          print "OK\n"
+        else
+          die()

commit 44305cb6b09e329dd085273d0535795f47ace4dd

Fix minor EmitTeX bug (introduced in 1032a64e1).
Use new cmp tool to check more elaborate munger example in
theory_tests directory.

src/TeX/EmitTeX.sml
@@ -434,25 +434,27 @@ let val {add_string,add_break,begin_block,add_newline,end_block,...} =
         TP (last (strip_type (hd l)))
       end
 
-    fun pp_constructor_spec (n, l) =
-        if !print_datatype_names_as_types then pp_type_name n
-        else if enumerated_type n then
-          (BB PP.CONSISTENT 0;
-           PT n; S " ";
-           BB PP.INCONSISTENT 0;
-           S "="; S " ";
-           app (fn x => (pp_clause x; BR(1,0); S"|"; S" "))
-               (List.take(l, length l - 1));
-           pp_clause(last l);
-           EB();
-           EB())
-        else
-          (BB PP.CONSISTENT 2;
-           PT n; S " "; S "="; BR(1,2);
-           app (fn x => (pp_clause x; BR(1,0); S "|"; S " "))
-               (List.take(l, length l - 1));
-           pp_clause (last l);
-           EB())
+    fun pp_constructor_spec (n, l) = let
+      val PT0 = if !print_datatype_names_as_types then pp_type_name else PT
+    in
+      if enumerated_type n then
+        (BB PP.CONSISTENT 0;
+         PT0 n; S " ";
+         BB PP.INCONSISTENT 0;
+         S "="; S " ";
+         app (fn x => (pp_clause x; BR(1,0); S"|"; S" "))
+             (List.take(l, length l - 1));
+         pp_clause(last l);
+         EB();
+         EB())
+      else
+        (BB PP.CONSISTENT 2;
+         PT0 n; S " "; S "="; BR(1,2);
+         app (fn x => (pp_clause x; BR(1,0); S "|"; S " "))
+             (List.take(l, length l - 1));
+         pp_clause (last l);
+         EB())
+    end
 
     fun pp_record_spec l =
         let val ll = tl l

tools/sequences/more-theories
@@ -1,6 +1,7 @@
 !src/boss/theory_tests
 !src/tfl/src/test
 src/TeX
+!src/TeX/theory_tests
 !examples/RSA
 src/sort
 src/string

src/TeX/theory_tests/.gitignore
@@ -1,0 +1,1 @@
+output

src/TeX/theory_tests/Holmakefile
@@ -1,0 +1,12 @@
+CMPDIR = $(protect $(HOLDIR)/tools/cmp)
+
+INCLUDES = $(CMPDIR)
+
+output: input munge.exe expected-output
+	./munge.exe < $< > $@
+	$(CMPDIR)/cmp.exe output expected-output
+
+munge.exe: mdtTheory.uo
+	$(protect $(HOLDIR)/bin/mkmunge.exe) mdtTheory
+
+EXTRA_CLEANS = output munge.exe

src/TeX/theory_tests/expected-output
@@ -1,0 +1,15 @@
+\HOLTyOp{term} =
+    \HOLConst{Var} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{type}
+  \HOLTokenBar{} \HOLConst{Const} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{type} \HOLTokenImp{} \HOLTyOp{const_tag}
+  \HOLTokenBar{} \HOLConst{Comb} \HOLKeyword{of} \HOLTyOp{term} \HOLTokenImp{} \HOLTyOp{term}
+  \HOLTokenBar{} \HOLConst{Abs} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{type} \HOLTokenImp{} \HOLTyOp{term} ;
+
+\HOLTyOp{type} = \HOLConst{Tyvar} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenBar{} \HOLConst{Tyapp} \HOLKeyword{of} \HOLTyOp{type_op} \HOLTokenImp{} \HOLTyOp{type} \HOLTyOp{list} ;
+
+\HOLTyOp{type_op} = \HOLConst{Typrim} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{num} \HOLTokenBar{} \HOLConst{Tydefined} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{term} ;
+
+\HOLTyOp{const_tag} =
+    \HOLConst{Prim}
+  \HOLTokenBar{} \HOLConst{Defined} \HOLKeyword{of} \HOLTyOp{num} \HOLTokenImp{} (\HOLTyOp{string} \HOLTokenProd{} \HOLTyOp{term}) \HOLTyOp{list} \HOLTokenImp{} \HOLTyOp{term}
+  \HOLTokenBar{} \HOLConst{Tyabs} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{term}
+  \HOLTokenBar{} \HOLConst{Tyrep} \HOLKeyword{of} \HOLTyOp{string} \HOLTokenImp{} \HOLTyOp{term}

src/TeX/theory_tests/input
@@ -1,0 +1,1 @@
+\HOLthm{mdt.datatype_term}

src/TeX/theory_tests/mdtScript.sml
@@ -1,0 +1,29 @@
+open HolKernel Parse boolLib bossLib;
+
+val _ = new_theory "mdt";
+
+val _ = new_type ("char", 0)
+
+val _ = type_abbrev("string", ``:char list``)
+
+val _ = Hol_datatype`
+  term = Var of string => type
+       | Const of string => type => const_tag
+       | Comb of term => term
+       | Abs of string => type => term
+       ;
+   type = Tyvar of string
+       | Tyapp of type_op => type list
+       ;
+   type_op =
+     Typrim of string => num
+   | Tydefined of string => term
+       ;
+   const_tag =
+     Prim
+   | Defined of num => (string # term) list => term
+   | Tyabs of string => term
+   | Tyrep of string => term
+`;
+
+val _ = export_theory();