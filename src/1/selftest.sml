HOL-Theorem-Prover/HOL

commit 1ffe3171fbad55dad341535f3bdc3cdd39d762b9

Test-case for github issue #172
The problem is that the pattern ``x: bool -> bool`` is matching
everything.

src/1/selftest.sml
@@ -403,6 +403,37 @@ val _ = test {input = "f /x",
               testf = (fn s => "Prefix op without parens: "^s),
               output = "f /x"}
 
+fun bfnprinter gs be sys ppfns gravs depth t = let
+  val (bvar, body) = dest_abs t
+in
+  if aconv bvar body then #add_string ppfns "I"
+  else if aconv body boolSyntax.T then #add_string ppfns "(K T)"
+  else if aconv body boolSyntax.F then #add_string ppfns "(K F)"
+  else if aconv body (mk_neg bvar) then #add_string ppfns "neg"
+  else raise term_pp_types.UserPP_Failed
+end handle HOL_ERR _ => raise term_pp_types.UserPP_Failed
+
+val _ = temp_add_user_printer("boolfunction", ``v : bool -> bool``,
+                              bfnprinter)
+
+val _ = test {input = "\\x:bool. x",
+              testf = (K "Boolean identity with special user printer"),
+              output = "I"}
+val _ = test {input = "\\x:'a. x",
+              testf = (K "Non-boolean identity with special user printer"),
+              output = "\\x. x"}
+val _ = test {
+      input = "\\x:bool. T",
+      testf = (K "Constant T with type :bool -> bool w/special user printer"),
+      output = "(K T)"
+    }
+val _ = test {
+      input = "\\x:'a. T",
+      testf = (K "Constant T with type :'a -> bool w/special user printer"),
+      output = "\\x. T"
+    }
+
+
 
 (* test DiskThms *)
 val _ = let

commit f2253bcc31a23c9f29f70c1527e845aa471ed82c

Get src/1/selftest.sml to compile under Moscow ML again.
Change in 1ffe317 (new test-case for issue #172) broke compilation
because Moscow ML's handling of record types differs from Poly/ML's.

src/1/selftest.sml
@@ -403,15 +403,16 @@ val _ = test {input = "f /x",
               testf = (fn s => "Prefix op without parens: "^s),
               output = "f /x"}
 
-fun bfnprinter gs be sys ppfns gravs depth t = let
-  val (bvar, body) = dest_abs t
-in
-  if aconv bvar body then #add_string ppfns "I"
-  else if aconv body boolSyntax.T then #add_string ppfns "(K T)"
-  else if aconv body boolSyntax.F then #add_string ppfns "(K F)"
-  else if aconv body (mk_neg bvar) then #add_string ppfns "neg"
-  else raise term_pp_types.UserPP_Failed
-end handle HOL_ERR _ => raise term_pp_types.UserPP_Failed
+fun bfnprinter gs be sys (ppfns : term_pp_types.ppstream_funs) gravs depth t =
+  let
+    val (bvar, body) = dest_abs t
+  in
+    if aconv bvar body then #add_string ppfns "I"
+    else if aconv body boolSyntax.T then #add_string ppfns "(K T)"
+    else if aconv body boolSyntax.F then #add_string ppfns "(K F)"
+    else if aconv body (mk_neg bvar) then #add_string ppfns "neg"
+    else raise term_pp_types.UserPP_Failed
+  end handle HOL_ERR _ => raise term_pp_types.UserPP_Failed
 
 val _ = temp_add_user_printer("boolfunction", ``v : bool -> bool``,
                               bfnprinter)