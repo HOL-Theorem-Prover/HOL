f2ece1c69250bc11451fc88df6b9e6e6591ff30c
Michael Norrish <michael.norrish@nicta.com.au>
2014-10-15 13:43:00 +1100

    Add emacs binding for ≰ symbol in hol-unicode.el

--- tools/hol-unicode.el
+++ tools/hol-unicode.el
@@ -62,6 +62,7 @@
 (define-key hol-unicode-not-map ":" "∉")
 (define-key hol-unicode-not-map "0" "∅")
 (define-key hol-unicode-not-map "~" "≁")
+(define-key hol-unicode-not-map (kbd "C-S-q") "≰")
 
 (define-key hol-unicode-subscript-map "1" "₁")
 (define-key hol-unicode-subscript-map "2" "₂")
