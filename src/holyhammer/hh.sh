a11860fd3fb7527b9b0cc68f4793ebab12e5232e
Thibault Gauthier <thibault.gauthier@uibk.ac.at>
2015-02-17 22:18:50 +0100

    fixing paths in hh.sh

--- src/holyhammer/hh.sh
+++ src/holyhammer/hh.sh
@@ -7,13 +7,13 @@ Theorem="Theorem"
 # Run eprover on 128 premises
 if [ -f $EPROVER ]; then
   sh hh_eprover.sh
-  EPROVER_STATUS=$(head -n 1 eprover/eprover_files/eprover_status)
+  EPROVER_STATUS=$(head -n 1 provers/eprover/eprover_files/eprover_status)
 fi
 
 # Run vampire on 96 premises
 if [ "$EPROVER_STATUS" != "$Theorem" ] && [ -f $VAMPIRE ]; then
   sh hh_vampire.sh
-  VAMPIRE_STATUS=$(head -n 1 vampire/vampire_files/vampire_status)
+  VAMPIRE_STATUS=$(head -n 1 provers/vampire/vampire_files/vampire_status)
 fi
 
 # Run z3 on 32 premises
