b41853f3d388ff87e7d6b3d5cc98e7b3951d4c43
Thibault Gauthier <thibault.gauthier@uibk.ac.at>
2015-03-01 06:25:03 +0100

    Error messages

--- src/holyhammer/hh_vampire.sh
+++ src/holyhammer/hh_vampire.sh
@@ -10,7 +10,7 @@ clean $VAMPIRE_DIR
 # Premise selection and translation
 ./hh_h4 96 ../theories ../theories/conjecture conjecture \
 $VAMPIRE_DIR > /dev/null || \
-(echo "Holyhammer binaries not found. Please run make in src/holyhammer"; exit)
+(echo "Please run make in src/holyhammer."; exit)
 # Run vampire on 96 premises
 cd ../provers
 sh vampire.sh
