415b814194645130098237c044376f1d2a621c4b
Thibault Gauthier <thibault.gauthier@uibk.ac.at>
2015-03-20 18:55:43 +0100

    Fixing Press Enter issue

--- developers/travis/before_install.sh
+++ developers/travis/before_install.sh
@@ -42,6 +42,6 @@ echo "*** Installing Ocaml ***"
 sudo apt-get install ocaml
 # installing g++-4.8
 echo "*** Installing g++-4.8 ***"
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install g++-4.8
