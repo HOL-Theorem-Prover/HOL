834fa9d863b54cd720c1e485080f1e7559266364
Thibault Gauthier <thibault.gauthier@uibk.ac.at>
2015-08-24 00:21:34 +0200

    holyhammer: removing some unnecessary bash variables in hh.sh

--- src/holyhammer/scripts/hh.sh
+++ src/holyhammer/scripts/hh.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-EPROVER="../provers/eprover/eprover"
-VAMPIRE="../provers/vampire/vampire"
-Z3="../provers/z3/z3"
-
 # Run eprover, vampire and z3
 (type eprover >/dev/null 2>&1 && sh hh_eprover.sh $2 $5 $1) & \
 (type vampire >/dev/null 2>&1 && sh hh_vampire.sh $3 $6 $1) & \
