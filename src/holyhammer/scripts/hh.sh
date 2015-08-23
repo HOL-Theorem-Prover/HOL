cb68734363dd30582d0df677e36a46bcbccee469
Thibault Gauthier <thibault.gauthier@uibk.ac.at>
2015-08-24 00:20:05 +0200

    holyhammer: waiting for all provers to finish

--- src/holyhammer/scripts/hh.sh
+++ src/holyhammer/scripts/hh.sh
@@ -7,4 +7,5 @@ Z3="../provers/z3/z3"
 # Run eprover, vampire and z3
 (type eprover >/dev/null 2>&1 && sh hh_eprover.sh $2 $5 $1) & \
 (type vampire >/dev/null 2>&1 && sh hh_vampire.sh $3 $6 $1) & \
-(type z3      >/dev/null 2>&1 && sh hh_z3.sh $4 $7 $1)
+(type z3      >/dev/null 2>&1 && sh hh_z3.sh $4 $7 $1) & \
+wait
