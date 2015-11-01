4da6df8c8a7ffd300aedfc85280f1a3657c3f9bb
Ramana Kumar <ramana@member.fsf.org>
2015-11-02 08:18:25 +1100

    initial work on adding compatibility layer for combinTheory
    will need to also add a theory file (akin to HOL4boolTheory) to rephrase
    the remaining 6 assumptions in terms of what the standard library
    provides.

---
+++ src/opentheory/compat/hol-combin.thy
@@ -0,0 +1,12 @@
+name: hol-combin
+version: 1.0
+description: Interface between OpenTheory Standard Library and HOL4 combinTheory
+author: Ramana Kumar <ramana@member.fsf.org>
+license: GPL
+requires: bool
+requires: hol-bool
+requires: function
+show: "Data.Bool"
+main {
+  article: "../../combin/combin.ot.art"
+}
