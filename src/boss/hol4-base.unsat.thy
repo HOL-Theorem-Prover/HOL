13256742424ab4086fa56a4f621bcd2ac42b9b00
Ramana Kumar <ramana@member.fsf.org>
2016-03-20 19:45:00 +1100

    Add targets for: OT base thms, unsat. HOL4 assums
    The aim is to create an environment for proving the HOL4 base
    assumptions using only the OT base theorems. The plan is to read the OT
    theorems from an article as axioms, then read the HOL4 base assums from
    an article as goals, and then write and record their proofs.

---
+++ src/boss/hol4-base.unsat.thy
@@ -0,0 +1,16 @@
+name: HOL4 Base
+version: 1.0
+description: HOL4 Basic theories
+author: HOL4 people
+license: BSD
+main {
+  import: base
+  import: hol
+}
+base {
+  package: base-1.200
+}
+hol {
+  import: base
+  package: hol-base-1.0
+}
