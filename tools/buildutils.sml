HOL-Theorem-Prover/HOL

commit 80833d88845a505c44b78f9a200e1e32d84166f8

Implement -j option for build
This is passed onto the Holmake calls that build makes. It is also made
persistent from invocation to invocation, just as sequence, kernel and
theory-graph choices are.

tools/buildutils.sml
@@ -39,6 +39,7 @@ val DYNLIB = Systeml.DYNLIB
 fun SYSTEML clist = Process.isSuccess (Systeml.systeml clist)
 
 val dfltbuildseq = fullPath [HOLDIR, "tools", "build-sequence"]
+val dfltjobnum = 4
 
 val help_mesg = let
   val istrm = TextIO.openIn (fullPath [HOLDIR, "tools", "buildhelp.txt"])
@@ -277,6 +278,38 @@ in
               end
 end
 
+fun deljopt numseen list =
+  let
+    fun maybewarn () =
+      if numseen = 1 then
+        warn "Multiple -j options; taking last one\n"
+      else ()
+    fun handle_string s rest =
+      let
+        val _ = maybewarn()
+        val (optval, rest) = deljopt (numseen + 1) rest
+        val n_opt = case Int.fromString s of
+                        NONE => (warn ("Ignoring bogus 'number' for -j: '"^
+                                       s ^ "'");
+                                 NONE)
+                      | x => x
+      in
+        case (optval, n_opt) of
+            (SOME _, _) => (optval, rest)
+          | (NONE, _) => (n_opt, rest)
+      end
+  in
+    case list of
+        [] => (NONE, [])
+      | ["-j"] => (warn "Trailing -j command-line option ignored";
+                   (NONE, []))
+      | "-j"::ns::t => handle_string ns t
+      | s::rest =>
+          if String.isPrefix "-j" s then
+            handle_string (String.extract(s,2,NONE)) rest
+          else deljopt numseen rest
+  end
+
 fun orlist slist =
     case slist of
       [] => ""
@@ -287,14 +320,14 @@ fun orlist slist =
 datatype cline_action =
          Clean of string
        | Normal of {kernelspec : string,
+                    jobcount : int,
                     seqname : string,
                     rest : string list,
                     build_theory_graph : bool}
 exception DoClean of string
 fun get_cline kmod = let
-  val reader = TextIO.inputLine
   (* handle -fullbuild vs -seq fname, and -expk vs -otknl vs -stdknl *)
-  val oldopts = read_earlier_options reader
+  val oldopts = read_earlier_options TextIO.inputLine
   val newopts = CommandLine.arguments()
   fun unary_toggle optname dflt f toggles opts =
       case inter toggles opts of
@@ -350,13 +383,29 @@ fun get_cline kmod = let
       unary_toggle "theory-graph" true (fn x => x = "-graph")
                    ["-graph", "-nograph"] newopts
   val bgoption = if buildgraph then [] else ["-nograph"]
+  val (jcount, newopts) =
+      case deljopt 0 newopts of
+          (NONE, new') =>
+          let
+          in
+            case deljopt 0 oldopts of
+                (NONE, _) => (dfltjobnum, new')
+              | (SOME jn, _) =>
+                if jn = dfltjobnum then (jn, new')
+                else (warn ("*** Using -j "^Int.toString jn^
+                            " from earlier build command; use -j to override");
+                      (jn, new'))
+          end
+        | (SOME jn, new') => (jn, new')
+  val joption = "-j" ^ Int.toString jcount
   val _ =
       if seqspec = dfltbuildseq then
-        write_options (knlspec::bgoption)
+        write_options (knlspec::joption::bgoption)
       else
-        write_options (knlspec::"-seq"::seqspec::bgoption)
+        write_options (knlspec::"-seq"::seqspec::joption::bgoption)
 in
   Normal {kernelspec = knlspec, seqname = seqspec, rest = newopts,
+          jobcount = jcount,
           build_theory_graph = buildgraph}
 end handle DoClean s => Clean s
 
@@ -798,7 +847,7 @@ fun process_cline kmod =
       in
         (full_clean SRCDIRS action; Process.exit Process.success)
       end
-    | Normal {kernelspec, seqname, rest, build_theory_graph} => let
+    | Normal {kernelspec, seqname, rest, build_theory_graph, jobcount} => let
         val (do_selftests, rest) = cline_selftest rest
         val SRCDIRS = read_buildsequence {kernelname = kernelspec} seqname
       in
@@ -807,6 +856,7 @@ fun process_cline kmod =
            Process.exit Process.success)
         else
           {cmdline=rest,build_theory_graph=build_theory_graph,
+           jobcount = jobcount,
            do_selftests = do_selftests, SRCDIRS = SRCDIRS}
       end
 

tools-poly/build.sml
@@ -21,7 +21,7 @@ datatype phase = Initial | Bare | Full
     Analysing the command-line
    ---------------------------------------------------------------------- *)
 
-val {cmdline,build_theory_graph,do_selftests,SRCDIRS} =
+val {cmdline,build_theory_graph,do_selftests,SRCDIRS,jobcount} =
   process_cline (fn c => c)
 
 open Systeml;
@@ -63,7 +63,9 @@ val Holmake = let
                       SysWord.toString (Posix.Signal.toWord sg)
   end
 in
-  buildutils.Holmake aug_systeml isSuccess phase_extras analysis do_selftests
+  buildutils.Holmake aug_systeml isSuccess
+                     (fn () => ("-j"^Int.toString jobcount) :: phase_extras())
+                     analysis do_selftests
 end
 
 (* create a symbolic link - Unix only *)

tools/build.sml
@@ -23,7 +23,7 @@ open buildutils
     Analysing the command-line
    ---------------------------------------------------------------------- *)
 
-val {cmdline,build_theory_graph,do_selftests,SRCDIRS} =
+val {cmdline,build_theory_graph,do_selftests,SRCDIRS,jobcount} =
     process_cline (fn s => s)
 
 open Systeml;
@@ -32,7 +32,8 @@ val Holmake = let
   fun sysl p args = Systeml.systeml (p::args)
   val isSuccess = OS.Process.isSuccess
 in
-  buildutils.Holmake sysl isSuccess (fn () => []) (fn _ => "") do_selftests
+  buildutils.Holmake sysl isSuccess (fn () => ["-j"^Int.toString jobcount])
+                     (fn _ => "") do_selftests
 end
 
 (* ----------------------------------------------------------------------

tools/buildutils.sig
@@ -16,6 +16,7 @@ sig
   val process_cline :
       (string -> string) ->
       {cmdline : string list, build_theory_graph : bool,
+       jobcount : int,
        do_selftests : int, SRCDIRS : (string * int) list}
 
   val map_dir : (string * string -> unit) -> string -> unit

commit 62ed9852d4b1a2ae0582a2d8fbcd39c3317cd137

Get build to honour selftest commandline argument
This behaviour was broken by 80833d8884

tools/buildutils.sml
@@ -279,36 +279,31 @@ in
               end
 end
 
-fun deljopt numseen list =
+fun deljopt list =
   let
-    fun maybewarn () =
-      if numseen = 1 then
-        warn "Multiple -j options; taking last one\n"
-      else ()
-    fun handle_string s rest =
-      let
-        val _ = maybewarn()
-        val (optval, rest) = deljopt (numseen + 1) rest
-        val n_opt = case Int.fromString s of
-                        NONE => (warn ("Ignoring bogus 'number' for -j: '"^
-                                       s ^ "'");
-                                 NONE)
-                      | x => x
-      in
-        case (optval, n_opt) of
-            (SOME _, _) => (optval, rest)
-          | (NONE, _) => (n_opt, rest)
-      end
-  in
-    case list of
-        [] => (NONE, [])
-      | ["-j"] => (warn "Trailing -j command-line option ignored";
-                   (NONE, []))
-      | "-j"::ns::t => handle_string ns t
-      | s::rest =>
+    fun maybewarn ns =
+      if ns = 1 then warn "Multiple -j options; taking last one\n" else ()
+    fun handle_string s =
+      case Int.fromString s of
+          NONE => (warn ("Ignoring bogus 'number' for -j: '"^ s ^ "'");
+                   NONE)
+        | x => x
+    fun recurse numseen list nopt args =
+      case list of
+          [] => (nopt, List.rev args)
+        | ["-j"] => (warn "Trailing -j command-line option ignored";
+                     (nopt, List.rev args))
+        | "-j"::ns::t =>
+          (maybewarn numseen; recurse (numseen + 1) t (handle_string ns) args)
+        | s::rest =>
           if String.isPrefix "-j" s then
-            handle_string (String.extract(s,2,NONE)) rest
-          else deljopt numseen rest
+            (maybewarn numseen;
+             recurse (numseen + 1) rest
+                     (handle_string (String.extract(s,2,NONE)))
+                     args)
+          else recurse numseen rest nopt (s::args)
+  in
+    recurse 0 list NONE []
   end
 
 fun orlist slist =
@@ -385,11 +380,11 @@ fun get_cline kmod = let
                    ["-graph", "-nograph"] newopts
   val bgoption = if buildgraph then [] else ["-nograph"]
   val (jcount, newopts) =
-      case deljopt 0 newopts of
+      case deljopt newopts of
           (NONE, new') =>
           let
           in
-            case deljopt 0 oldopts of
+            case deljopt oldopts of
                 (NONE, _) => (dfltjobnum, new')
               | (SOME jn, _) =>
                 if jn = dfltjobnum then (jn, new')