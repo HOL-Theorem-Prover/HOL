f0de5f6336cfbc9f5725c662deca79bcae22c2ef
Ramana Kumar <ramana@member.fsf.org>
2016-05-08 11:10:19 +1000

    Add OpenTheory package for integer

---
+++ src/integer/hol4-integer.thy
@@ -0,0 +1,23 @@
+name: hol-integer
+version: 1.0
+description: HOL integer theories
+author: HOL developers <hol-developers@lists.sourceforge.net>
+license: MIT
+requires: base
+requires: hol-base
+requires: hol-words
+requires: hol-string
+requires: hol-ring
+requires: hol-quotient
+show: "HOL4"
+show: "Data.Bool"
+show: "Data.List"
+show: "Data.Pair"
+show: "Data.Option"
+show: "Function"
+show: "Number.Natural"
+show: "Relation"
+main {
+  article: "hol4-integer-unint.art"
+  interpretation: "../opentheory/hol4.int"
+}
