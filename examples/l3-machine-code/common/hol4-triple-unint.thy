238afb0a38e8fa8c3f332d7421f7c994cd98b51e
Ramana Kumar <ramana@member.fsf.org>
2016-08-16 16:32:11 +1000

    Fix up OpenTheory package for tripleTheory
    Rename to match existing conventions better, and remove unnecessary
    -unint package.

--- examples/l3-machine-code/common/hol4-triple-unint.thy
+++
@@ -1,8 +0,0 @@
-name: hol-triple
-version: 1.0
-description: triple
-author: HOL OpenTheory Packager <opentheory-packager@hol-theorem-prover.org>
-license: MIT
-main {
-  article: "triple.ot.art"
-}
--- examples/l3-machine-code/common/machine-code-hoare-triple.thy
+++ examples/l3-machine-code/common/machine-code-hoare-triple.thy
@@ -1,13 +1,13 @@
-name: hol-triple
+name: machine-code-hoare-triple
 version: 1.0
-description: triple
+description: Total correctness machine-code Hoare triple
 author: HOL OpenTheory Packager <opentheory-packager@hol-theorem-prover.org>
 license: MIT
 requires: base
 requires: hol-base
-requires: hol-machine-code-hoare-triple
+requires: machine-code-hoare-logic
 show: "Data.Bool"
 main {
-  article: "triple-unint.art"
+  article: "triple.ot.art"
   interpretation: "../../../src/opentheory/hol4.int"
 }
