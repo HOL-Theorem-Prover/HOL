HOL-Theorem-Prover/HOL

commit 6828af9a214f63785dd3ae92b36b48600f0b2d71

Make final tag lines prettier when running -j n>4
Running something like Holmake -j8 works OK on a terminal with lots of
columns except that verdict lines used to retain junk text off to the
right of the OK. This can be fixed by using ANSI codes to clear to the
end of the line when printing the final "OK" (or "FAILED" etc).

tools/Holmake/poly/MB_Monitor.sml
@@ -86,6 +86,8 @@ val cheat_string = "Saved CHEAT _"
 val oracle_string = "Saved ORACLE thm _"
 val used_cheat_string = "(used CHEAT)"
 
+val CLR_EOL = "\027[0K" (* ANSI clear to EOL code *)
+
 fun new {info,warn,genLogFile,keep_going,time_limit} =
   let
     val monitor_map = ref (Binarymap.mkDict String.compare)
@@ -102,7 +104,7 @@ fun new {info,warn,genLogFile,keep_going,time_limit} =
           (Binarymap.app (fn (k,(_,v)) =>
                              print (polish k ^ statusString v))
                          (!monitor_map);
-           print "\027[0K" (* ANSI clear to EOL code *))
+           print CLR_EOL)
         else
           case Binarymap.listItems (!monitor_map) of
               [] => ()
@@ -114,7 +116,7 @@ fun new {info,warn,genLogFile,keep_going,time_limit} =
                        (case stat of
                             Stalling _ => statusString stat
                           | _ => "   "));
-                print "\027[0K" (* ANSI clear to EOL code *)
+                print CLR_EOL
               end
       end
     fun id (s:string) = s
@@ -130,7 +132,7 @@ fun new {info,warn,genLogFile,keep_going,time_limit} =
         | SOME info => f info
     fun taginfo tag colour s =
       info (infopfx ^ StringCvt.padRight #" " (80 - String.size s) tag ^
-            colour s)
+            colour s ^ CLR_EOL)
     fun monitor msg =
       case msg of
           StartJob (_, tag) =>

commit ffed319b5ad137dda9373ed451c1e09ca14d59d5

Ensure ANSI CLR_EOL code doesn't go to non-ttys
Without this, 6828af9a21 caused a regression

tools/Holmake/poly/MB_Monitor.sml
@@ -120,12 +120,13 @@ fun new {info,warn,genLogFile,keep_going,time_limit} =
               end
       end
     fun id (s:string) = s
-    val (startmsg, infopfx, display_map, green, red, boldyellow, dim) =
+    val (startmsg, infopfx, display_map, green, red, boldyellow, dim, CLR_EOL) =
         if strmIsTTY TextIO.stdOut then
-          ((fn s => ()), "\r", ttydisplay_map, green, red, boldyellow, dim)
+          ((fn s => ()), "\r", ttydisplay_map, green, red, boldyellow, dim,
+           CLR_EOL)
         else
           ((fn s => info ("Starting work on " ^ s)), "", (fn () => ()),
-           id, id, id, id)
+           id, id, id, id, "")
     fun stdhandle tag f =
       case Binarymap.peek (!monitor_map, tag) of
           NONE => (warn ("Lost monitor info for "^tag); NONE)