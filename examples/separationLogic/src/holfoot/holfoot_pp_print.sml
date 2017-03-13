HOL-Theorem-Prover/HOL

commit 5d5a8494a056ee164291691d18ee413623368152

Preserve add_user_printer properly in grammar data
This requires a slightly involved process for storing references to
code in theory files.

Update the .doc file on add_user_printer (which was already incredibly
long), and add a test.

Closes #367

examples/separationLogic/src/holfoot/holfoot_pp_print.sml
@@ -1331,7 +1331,6 @@ val pretty_printer_list =
 val use_holfoot_pp = ref true
 val _ = Feedback.register_btrace ("use holfoot_pp", use_holfoot_pp);
 
-
 fun trace_user_printer (up:term_grammar.userprinter) =
    (fn GS => fn sys => fn ppfns => fn gravs => fn d => fn pps => fn t =>
    if (!use_holfoot_pp) then
@@ -1344,7 +1343,12 @@ fun trace_user_printer (up:term_grammar.userprinter) =
 
 val pretty_printer_list_trace = map (fn (s, t, p) =>
    (s, t, trace_user_printer p)) pretty_printer_list
+val _ = app (fn (s,_,c) = term_grammar.userSyntaxFns.register_userPP
+                            {name = s, code = c})
+            pretty_printer_list_trace
 
+fun aup (s,pat,code) =
+   (add_ML_dependency "holfoot_pp_print"; add_user_printer(s,pat))
 
 fun temp_add_holfoot_pp () =
    (map temp_add_user_printer pretty_printer_list_trace;
@@ -1355,7 +1359,7 @@ fun temp_remove_holfoot_pp () =
     print "HOLFOOT pretty printing deactivated!\n");
 
 fun add_holfoot_pp_quiet () =
-   (map add_user_printer pretty_printer_list_trace;());
+   (map aup pretty_printer_list_trace;());
 
 fun add_holfoot_pp () =
     (add_holfoot_pp_quiet();

examples/separationLogic/src/holfoot/EXAMPLES/vstte/vscomp5.hol
@@ -124,7 +124,7 @@ fun amortized_queue_printer GS backend (sys_raw : term_pp_types.sysprinter) (ppf
     )
   end;
 
-val _ = add_user_printer ("amortized_list_printer", ``x:'a set``, amortized_queue_printer);
+val _ = temp_add_user_printer ("amortized_list_printer", ``x:'a set``, amortized_queue_printer);
 
 
 (******************************************)

help/Docfiles/Parse.add_user_printer.doc
@@ -1,6 +1,6 @@
 \DOC add_user_printer
 
-\TYPE {add_user_printer : (string * term * userprinter) -> unit}
+\TYPE {add_user_printer : (string * term) -> unit}
 
 \SYNOPSIS
 Adds a user specified pretty-printer for a specified type.
@@ -14,13 +14,22 @@ Parse
 \DESCRIBE
 
 The function {add_user_printer} is used to add a special purpose term
-pretty-printer to the interactive system.  The pretty-printer is
-called whenever the term to be printed matches (with {match_term}) the
-term provided as the second parameter of the triple.  If multiple
-calls to {add_user_printer} are made with the same string parameter,
-the older functions are replaced entirely.  If multiple printers
-match, the more specific match will be chosen.  If two matches are
-equally specific, the match chosen is unspecified.
+pretty-printer to the interactive system. The pretty-printer is called
+whenever the term to be printed matches (with {match_term}) the term
+provided as the second parameter. If multiple calls to
+{add_user_printer} are made with the same string parameter, the older
+functions are replaced entirely. If multiple printers match, the more
+specific match will be chosen. If two matches are equally specific,
+the match chosen is unspecified.
+
+The function that performs the printing is not given directly, but is
+instead referred to by name (the first parameter to
+{add_user_printer}). This name must be linked to the desired code with
+a call to {term_grammar.userSyntaxFns.register_userPP}, which function
+should be called within another ML file (i.e., not the ``Script’’ file
+of the theory). The name passed to the {register_userPP} function is
+then the name that must also be passed to {add_user_printer}. The term
+argument is the desired pattern.
 
 The user-supplied function may choose not to print anything for the
 given term and hand back control to the standard printer by raising
@@ -45,7 +54,7 @@ where the type {grav} (from {term_pp_types}) is
 {
    datatype grav = Top | RealTop | Prec of (int * string)
 }
-The type {uprinter} (standing for "unit printer") is a special monadic
+The type {uprinter} (standing for ``unit printer’’) is a special monadic
 printing type based on the {smpp} module (explained further in the
 example below).  The type {sysprinter} is another abbreviation
 {
@@ -54,7 +63,7 @@ example below).  The type {sysprinter} is another abbreviation
        depth : int } -> term -> uprinter
 }
 Thus, when the user's printing function is called, it is passed ten
-parameters, including three "gravity" values in a triple, and two
+parameters, including three ``gravity’’ values in a triple, and two
 grammars.  The fourth parameter is the system's own printer. The fifth
 parameter is a record of functions to call for adding a string to the
 output, adding a break, adding new lines, defining some styles for
@@ -135,24 +144,49 @@ elsewhere. This logic ensures that a term like {\x. x} prints as
 {\x:'a. x}. The first, binding occurrence of the variable gets an
 annotation; subsequent occurrences do not.
 
-Finally, the string parameter to the {add_user_printer} function is a
-string corresponding to the ML function.  Best practice is probably to
-define the printing function in an independent structure and to then
-have the string be of the form {"module.fnname"}.  This parameter is
-not present in the accompanying {temp_add_user_printer}, as this
-latter function does not affect the grammar exported to disk with
-{export_theory}.
-
 \FAILURE
-Will not fail directly, but if the function parameter fails to print
-all terms of the registered type in any other way than raising the
-{UserPP_Failed} exception, then the pretty-printer will also fail.  If
-the string parameter does not correspond to valid ML code, then the
-theory file generated by {export_theory} will not compile.
+Fails if the string parameter does not correspond to a name used to
+register a function with {term_grammar.userSyntaxFns.register_userPP}.
+In addition, if the function parameter fails to print all terms of the
+registered type in any other way than raising the {UserPP_Failed}
+exception, then the pretty-printer will also fail.
 
 \EXAMPLE
-This example uses the system printer to print sub-terms, and concerns
-itself only with printing conjunctions.  Note how the actions that make up the pretty-printer (combinations of {add_string} and {add_break} are combined with the infix {>>} operator (from the {smpp} module).
+
+In the examples that follow, the companion {temp_add_userprinter}
+function is used: this function takes a value of type {userprinter}
+directly, and so is a more direct demonstration of how user-printers
+can behave. The boilerplate required for
+preserved-across-{export_theory} functionality starts with the
+external module. For a theory {fooScript.sml}, one might write the file {fooPP.sml}:
+{
+   structure fooPP =
+   struct
+
+     fun term_printer_code ... = ...
+     val _ = term_grammar.userSyntaxFns.register_userPP {
+               name = "foo.term_printer", code = term_printer_code
+             }
+   end
+}
+where {term_printer_code} has type {userprinter}. Note that the code
+for the printer has to be written so that it can be compiled before
+the theory {foo} is in context. In particular, top-level calls to
+{mk_thy_const} and the like will fail if they attempt to bind
+constants declared in theory {foo}.
+
+In {fooScript.sml}, the following is the idiom required:
+{
+   local open fooPP in end;
+   val _ = add_ML_dependency "fooPP"
+   val _ = add_user_printer ("foo.term_printer", ``term pattern``)
+}
+
+As discussed, the remaining examples use {temp_add_user_printer}. The
+first example uses the system printer to print sub-terms, and concerns
+itself only with printing conjunctions. Note how the actions that make
+up the pretty-printer (combinations of {add_string} and {add_break}
+are combined with the infix {>>} operator (from the {smpp} module).
 {
   - fun myprint Gs B sys (ppfns:term_pp_types.ppstream_funs) gravs d t =
     let
@@ -196,7 +230,8 @@ arguments and cause the arguments to be printed with a particular
 governing style ({ustyle}), or indented to reveal block structure
 ({ublock}).
 {
-  - fun myprint2 Gs B sys (ppfns:term_pp_types.ppstream_funs) (pg,lg,rg) d t = let
+  - fun myprint2 Gs B sys (ppfns:term_pp_types.ppstream_funs) (pg,lg,rg) d t =
+    let
       open Portable term_pp_types PPBackEnd smpp
       val {add_string,add_break,ublock,ustyle,...} = ppfns
       val (l,r) = dest_conj t

src/1/selftest.sml
@@ -401,11 +401,13 @@ val _ = app tpp [
 ]
 
 open testutils
-val test = tpp_expected
+val condprinter_test = tpp_expected
              |> Lib.with_flag (linewidth,!Globals.linewidth)
              |> unicode_off
              |> raw_backend
-val _ = app test [
+val test = condprinter_test
+val condprinter_tests =
+    [
       {input = "if oless e1 e2 /\\ oless x y /\\ foobabbbbbbb\n\
                \then p /\\ q /\\ r /\\ ppppp xxxx yyyyy\n\
                \else if (e1 = e2) /\\ k1 <> k2\n\
@@ -428,7 +430,8 @@ val _ = app test [
                 \else if (e1 = e2) /\\ k1 <> k2 then T\n\
                 \else if (e1 = e2) /\\ (k1 = k2) /\\ oless t1 t2 then T\n\
                 \else F"}
-]
+  ]
+val _ = app condprinter_test condprinter_tests
 
 val _ = let
   open testutils
@@ -831,7 +834,10 @@ in
     | _ => die "FAILED"
 end
 
+val _ = if List.all substtest tests then ()
+        else die "Substitution test failed"
 
-
-val _ = Process.exit (if List.all substtest tests then Process.success
-                      else Process.failure)
+val _ = print "Testing cond-printer after set_grammar_ancestry\n"
+val _ = set_trace "PP.avoid_unicode" 1
+val _ = set_grammar_ancestry ["bool"]
+val _ = app condprinter_test condprinter_tests

src/bool/boolScript.sml
@@ -4241,4 +4241,8 @@ val _ = TeX_notation {hol="<=/=>", TeX = ("\\HOLTokenNotEquiv{}",2)}
 val _ = TeX_notation {hol=UChar.not_iff,
                       TeX = ("\\HOLTokenNotEquiv{}",2)}
 
+local open boolpp in end
+val _ = add_ML_dependency "boolpp"
+val _ = add_user_printer ("bool.COND", ``COND gd tr fl``)
+
 val _ = export_theory();

src/bool/boolpp.sig
@@ -1,0 +1,6 @@
+signature boolpp =
+sig
+
+  val condprinter : term_grammar.userprinter
+
+end

src/bool/boolpp.sml
@@ -1,13 +1,22 @@
-structure ParseExtras :> ParseExtras =
+structure boolpp :> boolpp =
 struct
 
-open Parse HolKernel boolSyntax
+open Parse HolKernel
 
-fun tight_equality() = set_fixity "=" (Infix(NONASSOC, 450))
-fun temp_tight_equality() = temp_set_fixity "=" (Infix(NONASSOC, 450))
-
-fun loose_equality () = set_fixity "=" (Infix(NONASSOC, 100))
-fun temp_loose_equality () = temp_set_fixity "=" (Infix(NONASSOC, 100))
+fun dest_cond tm =
+  let
+    val (f, args) = strip_comb tm
+    val {Thy,Name,...} = dest_thy_const f
+      handle HOL_ERR _ =>
+             raise mk_HOL_ERR "boolpp" "dest_cond" "Not even a const at head"
+  in
+    if Thy = "bool" andalso Name = "COND" then
+      case args of
+          [gd, th, el] => (gd,th,el)
+        | _ => raise mk_HOL_ERR "boolpp" "dest_cond" "Wrong number of arguments"
+    else
+      raise mk_HOL_ERR "boolpp" "dest_cond" "Not a COND-term"
+  end
 
 fun condprinter (tyg, tmg) backend printer ppfns (pgr,lgr,rgr) depth tm = let
   open term_pp_types smpp
@@ -63,5 +72,8 @@ in
     (doguard false (g,t) >> add_break(1,0) >> doelse e) >>
   doparen ")"
 end
+
+val _ = term_grammar.userSyntaxFns.register_userPP
+          {name = "bool.COND", code = condprinter}
 
 end

src/complex/complexPP.sml
@@ -1,0 +1,10 @@
+structure complexPP =
+struct
+
+val _ = term_grammar.userSyntaxFns.register_userPP
+          {name = "complex.decimalfractions",
+           code = DecimalFractionPP.fraction{Thy = "real",
+                                             Division = "/",
+                                             fromNum = "real_of_num"}}
+
+end

commit 9b6b61cfac912ff1f69625d42c40f770fe769a88

fix typo introduced by 5d5a8494a056ee164291691d18ee413623368152
there was a syntax error in examples/separationLogic that was introduced
when updating storing the pretty printer via
5d5a8494a056ee164291691d18ee413623368152

examples/separationLogic/src/holfoot/holfoot_pp_print.sml
@@ -1343,7 +1343,7 @@ fun trace_user_printer (up:term_grammar.userprinter) =
 
 val pretty_printer_list_trace = map (fn (s, t, p) =>
    (s, t, trace_user_printer p)) pretty_printer_list
-val _ = app (fn (s,_,c) = term_grammar.userSyntaxFns.register_userPP
+val _ = app (fn (s,_,c) => term_grammar.userSyntaxFns.register_userPP
                             {name = s, code = c})
             pretty_printer_list_trace
 