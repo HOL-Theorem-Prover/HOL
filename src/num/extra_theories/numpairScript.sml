HOL-Theorem-Prover/HOL

commit 76aa5cf593b31781750f8ab6edba34bc7a0e85be

Define nlistrec in numpair so as to export core def'n properly
The old-fashioned use of tprove and Hol_defn caused the underlying
definition to not be exported.  Now it's in the theory correctly, and
also used by computeLib (as we want).

src/num/extra_theories/numpairScript.sml
@@ -230,17 +230,13 @@ val ncons_not_nnil = Store_thm(
   ``ncons x y <> nnil``,
   SRW_TAC [ARITH_ss][ncons_def]);
 
-val nlistrec_defn = Defn.Hol_defn "nlistrec" `
+val nlistrec_defn = tDefine "nlistrec" `
   nlistrec n f l = if l = 0 then n
                    else f (nfst (l - 1)) (nsnd (l - 1))
                           (nlistrec n f (nsnd (l - 1)))
-`;
-
-val (nlistrec_def, nlistrec_ind) = Defn.tprove(
-  nlistrec_defn,
-  WF_REL_TAC `measure (SND o SND)` THEN
-  STRIP_TAC THEN ASSUME_TAC (Q.INST [`n` |-> `l - 1`] nsnd_le) THEN
-  DECIDE_TAC);
+` (WF_REL_TAC `measure (SND o SND)` THEN
+   STRIP_TAC THEN ASSUME_TAC (Q.INST [`n` |-> `l - 1`] nsnd_le) THEN
+   DECIDE_TAC);
 
 val nlistrec_thm = Store_thm(
   "nlistrec_thm",

commit 1378af236bf96428926de93d944ce3806f7f19be

Fix errors caused by careless commit in 76aa5cf

src/num/extra_theories/numpairScript.sml
@@ -230,13 +230,14 @@ val ncons_not_nnil = Store_thm(
   ``ncons x y <> nnil``,
   SRW_TAC [ARITH_ss][ncons_def]);
 
-val nlistrec_defn = tDefine "nlistrec" `
+val nlistrec_def = tDefine "nlistrec" `
   nlistrec n f l = if l = 0 then n
                    else f (nfst (l - 1)) (nsnd (l - 1))
                           (nlistrec n f (nsnd (l - 1)))
 ` (WF_REL_TAC `measure (SND o SND)` THEN
    STRIP_TAC THEN ASSUME_TAC (Q.INST [`n` |-> `l - 1`] nsnd_le) THEN
    DECIDE_TAC);
+val nlistrec_ind = theorem "nlistrec_ind"
 
 val nlistrec_thm = Store_thm(
   "nlistrec_thm",