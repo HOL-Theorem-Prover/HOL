6e611ac46946fb26e41e1eaebe0dd87c6a7ed692
Thibault Gauthier <thibault.gauthier@uibk.ac.at>
2018-03-27 14:15:23 +0200

    tactictoe: fixing stats.sh
    (cherry picked from commit b61154685dc3ebd28880fc337514deed05fe5a2d)

--- src/tactictoe/stats.sh
+++ src/tactictoe/stats.sh
@@ -12,3 +12,4 @@ TOTAL=$(grep -r "Proof" $1 | wc -l)
 FOUND=$(grep -r "Proof found" $1 | wc -l)
 SATUR=$(grep -r "Proof status: Saturated" $1 | wc -l)
 TIMOU=$(grep -r "Proof status: Time Out" $1 | wc -l)
+echo "$TOTAL $FOUND $SATUR $TIMOU"
