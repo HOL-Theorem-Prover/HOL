HOL-Theorem-Prover/HOL

commit 81b42b0ba8065281ae101db3cc8e0b75f373bde6

Add OPTION_BIND_def to computeLib functions, add a regression test in selftest.ml. Closes #557

src/coretypes/optionScript.sml
@@ -459,6 +459,7 @@ val OPTION_BIND_def = Prim_rec.new_recursive_definition
    def = Term`(OPTION_BIND NONE f = NONE) /\
               (OPTION_BIND (SOME x) f = f x)`}
 val _= export_rewrites ["OPTION_BIND_def"]
+val _ = computeLib.add_persistend_funs[OPTION_BIND_def];
 
 val OPTION_BIND_cong = Q.store_thm(
   "OPTION_BIND_cong",

src/coretypes/selftest.sml
@@ -17,6 +17,14 @@ val _ = if Type.compare(ty, alpha) = EQUAL then OK()
 
 val _ = tpp "case opt of NONE => T | SOME b => b"
 
+(* regression test for OPTION_BIND evaluation *)
+val _ =
+    let val _ = prove (``OPTION_BIND (SOME T) (\x. SOME x) = SOME T``, EVAL_TAC)
+      handle HOL_ERR msg => die "FAILED testing OPTION_BIND evaluation!";
+    in
+    OK()
+end;
+
 (* testing for pairs *)
 val die = fn () => die "FAILED!\n"
 fun sdie s = testutils.die ("FAILED!\n  "^s^"\n")

commit 46c46cd4c9f625ea1a034fa525be9d85eb28d46c

Fix typo and other errors
introduced in 81b42b0ba8065281ae101db3cc8e0b75f373bde6

src/coretypes/optionScript.sml
@@ -459,7 +459,7 @@ val OPTION_BIND_def = Prim_rec.new_recursive_definition
    def = Term`(OPTION_BIND NONE f = NONE) /\
               (OPTION_BIND (SOME x) f = f x)`}
 val _= export_rewrites ["OPTION_BIND_def"]
-val _ = computeLib.add_persistend_funs[OPTION_BIND_def];
+val _ = computeLib.add_persistent_funs["OPTION_BIND_def"];
 
 val OPTION_BIND_cong = Q.store_thm(
   "OPTION_BIND_cong",

src/coretypes/selftest.sml
@@ -19,7 +19,7 @@ val _ = tpp "case opt of NONE => T | SOME b => b"
 
 (* regression test for OPTION_BIND evaluation *)
 val _ =
-    let val _ = prove (``OPTION_BIND (SOME T) (\x. SOME x) = SOME T``, EVAL_TAC)
+    let val _ = prove (``OPTION_BIND (SOME T) (\x. SOME x) = SOME T``, computeLib.EVAL_TAC)
       handle HOL_ERR msg => die "FAILED testing OPTION_BIND evaluation!";
     in
     OK()