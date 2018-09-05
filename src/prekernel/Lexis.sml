HOL-Theorem-Prover/HOL

commit f42dfacb21e666720bc5220b6f2940957c6e2a4b

Reimplement variable name-gen operations in kernel with sync-vars

src/prekernel/Lexis.sml
@@ -238,14 +238,16 @@ end;
 local fun num2name s i = s^Lib.int_to_string i
       fun subscripts x s =
         let val project = num2name (s^x)
-            val cnt = ref 0
-            fun incr() = (cnt := !cnt + 1; project (!cnt))
-        in incr
+            val cnt = Portable.make_counter{init=0,inc=1}
+            fun incr() = project (cnt())
+        in
+          incr
         end
       fun primes s =
-        let val current = ref s
-            fun next () = (current := Lib.prime (!current); !current)
-        in next
+        let val {upd,...} = Portable.syncref s
+            fun next () = upd (fn s => let val s' = Lib.prime s in (s',s') end)
+        in
+          next
         end
 in
 fun nameStrm s =

src/0/Term.sml
@@ -320,10 +320,10 @@ fun mk_primed_var (Name,Ty) =
  *---------------------------------------------------------------------------*)
 
 local val genvar_prefix = "%%genvar%%"
-      fun num2name i = genvar_prefix^Lib.int_to_string i
-      val nameStrm = Lib.mk_istream (fn x => x+1) 0 num2name
+      fun num2name i = genvar_prefix^Int.toString i
+      val num_stream = Portable.make_counter{init=0,inc=1}
 in
-fun genvar ty = Fv(state(next nameStrm), ty)
+fun genvar ty = Fv(num2name(num_stream()), ty)
 
 fun genvars ty =
  let fun gen acc n = if n <= 0 then rev acc else gen (genvar ty::acc) (n-1)

src/0/Type.sml
@@ -300,10 +300,10 @@ fun compare (Tyv s1, Tyv s2) = String.compare (s1,s2)
  ---------------------------------------------------------------------------*)
 
 local val gen_tyvar_prefix = "%%gen_tyvar%%"
-      fun num2name i = gen_tyvar_prefix^Lib.int_to_string i
-      val nameStrm   = Lib.mk_istream (fn x => x+1) 0 num2name
+      fun num2name i = gen_tyvar_prefix ^ Int.toString i
+      val nameStrm   = Portable.make_counter{inc=1,init=0}
 in
-fun gen_tyvar () = Tyv(state(next nameStrm))
+fun gen_tyvar () = Tyv(num2name(nameStrm()))
 
 fun is_gen_tyvar (Tyv name) = String.isPrefix gen_tyvar_prefix name
   | is_gen_tyvar _ = false;

commit 0f8d5000c2cc9a3262455b71a801bd0259c3cc18

Fix subscript nameStreams to start at 1 rather than 0
This was broken by f42dfacb21e and proofs do depend on
this (examples/ARM/arm6-verification/correctness/correctScript.sml in
particular).

src/prekernel/Holmakefile
@@ -4,7 +4,7 @@ PORTABLED = $(dprot $(SIGOBJ)/Portable.ui)
 
 UOFILES = $(patsubst %.sml,%.uo,$(wildcard *.sml))
 
-all: $(UOFILES)
+all: $(UOFILES) selftest.exe
 
 FinalTag-sig.uo: FinalTag-sig.sml Nonce.ui $(PORTABLED)
 	$(HOLMOSMLC) -c -toplevel $<
@@ -20,3 +20,8 @@ FinalThm-sig.uo: FinalThm-sig.sml Nonce.ui Lib.uo $(PORTABLED)
 
 KNametab.uo: KNametab.sml $(dprot $(SIGOBJ)/Table.ui)
 	$(HOLMOSMLC) -c Table.ui $<
+
+selftest.exe: selftest.uo
+	$(HOLMOSMLC) -o $@ $<
+
+EXTRA_CLEANS = selftest.exe

src/prekernel/Lexis.sml
@@ -238,7 +238,7 @@ end;
 local fun num2name s i = s^Lib.int_to_string i
       fun subscripts x s =
         let val project = num2name (s^x)
-            val cnt = Portable.make_counter{init=0,inc=1}
+            val cnt = Portable.make_counter{init=1,inc=1}
             fun incr() = project (cnt())
         in
           incr

src/prekernel/selftest.sml
@@ -1,0 +1,11 @@
+fun tprint s = print (StringCvt.padRight #" " 75 s)
+fun die s = (print (s^"\n"); OS.Process.exit OS.Process.failure)
+
+val _ = tprint "nameStream in subscripting mode starts at 1";
+
+val _ = Globals.priming := SOME ""
+val f = Lexis.nameStrm "foo"
+val l = List.tabulate(10, fn i => f())
+val expected = List.tabulate(10, fn i => "foo" ^ Int.toString (i + 1))
+
+val _ = (l = expected before print "OK\n") orelse die "FAILED"