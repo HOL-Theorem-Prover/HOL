HOL-Theorem-Prover/HOL

commit ea1b9b0f59328d000a077e02df663f271f48be75

Use new Portable.reraise in Conv.testconv

src/1/Conv.sml
@@ -272,7 +272,7 @@ fun testconv (f:conv) x =
   SOME (SOME (f x))
   handle UNCHANGED => SOME NONE
        | HOL_ERR _ => NONE
-       | e => raise e
+       | e => Portable.reraise e
 
 fun IFC (conv1:conv) conv2 conv3 tm =
   case testconv conv1 tm of

commit 76194473b2249ceab539f6265399c7749c579965

Delete unnecessary code in Conv.testconv
Handling an exception only to reraise it is a no-op (brain fart
introduced by me in ea1b9b0f59).

src/1/Conv.sml
@@ -278,7 +278,6 @@ fun testconv (f:conv) x =
   SOME (SOME (f x))
   handle UNCHANGED => SOME NONE
        | HOL_ERR _ => NONE
-       | e => Portable.reraise e
 
 fun IFC (conv1:conv) conv2 conv3 tm =
   case testconv conv1 tm of