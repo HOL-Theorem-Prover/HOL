3f4ad1c1d5be4ec55862dafb037009858a1e5f2c
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2018-09-21 22:22:37 +1000

    Rewrite before_install.sh as install-poly.sh
    Promote the contents of before_install.sh to be the de facto way of
    setting up PolyML.
    
    To make arguments for the script less confusing, use "T" and "F"
    instead of empty/non-empty environment variables.
    Also rename the environment arguments to be prefixed by POLY instead
    of suffixed as I consider this more clear.
    
    The renaming is in conformance with other {verb}-{noun} constructs in
    /developers.

---
+++ developers/install-poly.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+POLY_BASE="https://github.com/polyml/polyml"
+
+# Defaults:
+POLY_VERSION=${POLY_VERSION:-v5.7.1}
+POLY_GIT=${POLY_GIT:-F}
+POLY_ROOT=${POLY_ROOT:-F}
+
+PREFIX=$HOME
+if [ "$POLY_ROOT" == "T" ]
+then
+  PREFIX="/usr/"
+fi
+
+if [ "$POLY_GIT" == "T" ]
+then
+  git clone ${POLY_BASE}.git
+else
+  mkdir polyml
+  wget -qO- ${POLY_BASE}/archive/${POLY_VERSION}.tar.gz | \
+  tar xvz -C polyml --strip-components 1
+fi
+
+cd polyml
+echo "*** Configuring PolyML for prefix: ${PREFIX}"
+./configure --prefix=$PREFIX
+make
+
+if [ "$POLY_GIT" == "T" ]
+then
+  make compiler
+fi
+
+if [ "$POLY_ROOT" == "T" -a "$UID" != "0" ]
+then
+  sudo make install
+else
+  make install
+fi
+
+if [ $(uname) = "Darwin" ]
+then
+  perl -pi -e 's/-R/-rpath /g' $HOME/bin/polyc
+fi
