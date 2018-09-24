HOL-Theorem-Prover/HOL

commit d71df4673686012978eac550b6fa5fcca0a5c5a1

Stop simplifier from generating rewrites on boolean variables
Occasionally, an assumption will be generated that the simplifier
reads as a conditional rewrite, but which has the shape

   !... x.....  preconds x y z ==> x

where x is a variable of type bool. These generate pointless
conditional rewrites for all possible terms of type bool.  Now we stop
these from getting in.

Closes #556

src/simp/src/Cond_rewr.sml
@@ -278,6 +278,7 @@ val FT_EQ_F = PROVE_HYP (UNDISCH_ALL (NOT_ELIM (CONJUNCT2 BOOL_EQ_DISTINCT)))
 
 fun IMP_EQ_CANON (thm,bnd) = let
   val conditions = #1 (strip_imp (concl thm))
+  val hypfvs = hyp_frees thm
   val undisch_thm = UNDISCH_ALL thm
   val conc = concl undisch_thm
   fun IMP_EQ_CANONb th = IMP_EQ_CANON (th, bnd)
@@ -298,11 +299,14 @@ fun IMP_EQ_CANON (thm,bnd) = let
               else [(CONV_RULE (REWR_CONV EQ_SYM_EQ) undisch_thm, bnd)]
             else
               let
+                fun safelhs t = not (is_var t) orelse type_of t <> bool orelse
+                                t IN hypfvs
                 val base =
                     if null (subtract (free_vars r) (free_varsl (l::hyp thm)))
+                       andalso safelhs l
                     then undisch_thm
                     else
-                      (trace(1,IGNORE("rewrite with existential vars (adding \
+                      (trace(1,IGNORE("rewrite with bad vars (adding \
                                       \EQT version(s))",thm));
                        EQT_INTRO undisch_thm)
                 val flip_eqp = let val (l,r) = dest_eq (concl base)
@@ -323,9 +327,16 @@ fun IMP_EQ_CANON (thm,bnd) = let
       else if is_forall conc then
         undisch_thm |> SPEC_VAR |> snd |> IMP_EQ_CANONb
       else if is_neg conc then
-        if is_eq (dest_neg conc) then
-          [(EQF_INTRO undisch_thm, bnd), (EQF_INTRO (GSYM undisch_thm), bnd)]
-        else [(EQF_INTRO undisch_thm, bnd)]
+        let
+          val n = dest_neg conc
+        in
+          if is_eq n then
+            [(EQF_INTRO undisch_thm, bnd), (EQF_INTRO (GSYM undisch_thm), bnd)]
+          else if is_var n andalso not (n IN hypfvs) then
+            (trace(1, IGNORE ("boolean variable conclusion", thm)); [])
+          else
+            [(EQF_INTRO undisch_thm, bnd)]
+        end
       else if conc = truth_tm then
         (trace(2,IGNORE ("pointless rewrite",thm)); [])
       else if conc = false_tm then [(MP x_eq_false undisch_thm, bnd)]
@@ -349,7 +360,10 @@ fun IMP_EQ_CANON (thm,bnd) = let
             end
           else []
         end
-      else [(EQT_INTRO undisch_thm, bnd)]
+      else if is_var conc andalso not (conc IN hypfvs) then
+        (trace(1, IGNORE ("boolean variable conclusion", thm)); [])
+      else
+        [(EQT_INTRO undisch_thm, bnd)]
 in
   map (fn (th,bnd) => (CONJ_DISCH conditions th, bnd)) undisch_rewrites
 end handle e => WRAP_ERR("IMP_EQ_CANON",e);

src/simp/src/selftest.sml
@@ -224,4 +224,29 @@ in
     | SOME _ => OK()
 end
 
+local
+fun testb (s, thm, c) =
+  let
+    val _ = tprint ("Cond_rewr.mk_cond_rewrs on "^s)
+  in
+    case Lib.total Cond_rewr.mk_cond_rewrs(thm, BoundedRewrites.UNBOUNDED)
+     of
+        NONE => die "FAILED!"
+      | SOME l => if length l = c then OK()
+                  else die ("\n  FAILED!  Incorrectly generated rewrites\n  " ^
+                            String.concatWith "\n  "
+                                              (map (thm_to_string o #1) l))
+  end
+val lem1 = prove(“a <> b ==> (a = ~b)”,
+                 ASM_CASES_TAC “a:bool” THEN ASM_REWRITE_TAC[])
+in
+val _ = app testb [
+  ("“hyp ==> b”", ASSUME “(!b x y. (P x y = b) ==> b)”, 0),
+  ("“hyp ==> ~b”", ASSUME “(!b x y. (p x y = b) ==> ~b)”, 0),
+  ("“hyp ==> b=e”", ASSUME “(!b:bool x y. (p x y = b) ==> (b = e))”, 2),
+  ("“a <> b ==> (a = ~b)", lem1, 2)
+]
+
+end (* local fun testb ... *)
+
 val _ = Process.exit Process.success

commit 5bb5b631f763a053237b3bb033cc929e772b4b89

Fix bug in handling of bounded simp (e.g., with Once)
The bug was introduced by d71df467 and was apparent in a CakeML proof:
the commit made it impossible to use the simplifier to rewrite with
the Abbrev definition backwards (i.e., GSYM markerTheory.Abbrev_def)
because the variable on the LHS of that theorem has type bool. This is
a loopy rewrite, but should be permitted if the rewrite is bounded.

Equally, rewriting with the definition of I backwards would exhibit
the same problem, as long as one first instantiated the alpha in that
theorem to bool.

src/simp/src/Cond_rewr.sml
@@ -299,8 +299,9 @@ fun IMP_EQ_CANON (thm,bnd) = let
               else [(CONV_RULE (REWR_CONV EQ_SYM_EQ) undisch_thm, bnd)]
             else
               let
-                fun safelhs t = not (is_var t) orelse type_of t <> bool orelse
-                                t IN hypfvs
+                fun safelhs t =
+                  not (is_var t) orelse type_of t <> bool orelse
+                  t IN hypfvs orelse bnd <> BoundedRewrites.UNBOUNDED
                 val base =
                     if null (subtract (free_vars r) (free_varsl (l::hyp thm)))
                        andalso safelhs l

src/simp/src/selftest.sml
@@ -225,28 +225,41 @@ in
 end
 
 local
+  fun die_r l =
+    die ("\n  FAILED!  Incorrectly generated rewrites\n  " ^
+         String.concatWith "\n  " (map (thm_to_string o #1) l))
 fun testb (s, thm, c) =
   let
     val _ = tprint ("Cond_rewr.mk_cond_rewrs on "^s)
   in
     case Lib.total Cond_rewr.mk_cond_rewrs(thm, BoundedRewrites.UNBOUNDED)
      of
-        NONE => die "FAILED!"
-      | SOME l => if length l = c then OK()
-                  else die ("\n  FAILED!  Incorrectly generated rewrites\n  " ^
-                            String.concatWith "\n  "
-                                              (map (thm_to_string o #1) l))
+        NONE => die "EXN-FAILED!"
+      | SOME l => if length l = c then OK() else die_r l
   end
 val lem1 = prove(“a <> b ==> (a = ~b)”,
                  ASM_CASES_TAC “a:bool” THEN ASM_REWRITE_TAC[])
+val marker = GSYM markerTheory.Abbrev_def
 in
 val _ = app testb [
   ("“hyp ==> b”", ASSUME “(!b x y. (P x y = b) ==> b)”, 0),
   ("“hyp ==> ~b”", ASSUME “(!b x y. (p x y = b) ==> ~b)”, 0),
   ("“hyp ==> b=e”", ASSUME “(!b:bool x y. (p x y = b) ==> (b = e))”, 2),
-  ("“a <> b ==> (a = ~b)", lem1, 2)
+  ("“a <> b ==> (a = ~b)", lem1, 2),
+  ("x = Abbrev x", marker, 2)
 ]
 
+val _ = tprint "Cond_rewr.mk_cond_rewrs on bounded x <=> Abbrev x"
+val _ = let
+  val b = BoundedRewrites.BOUNDED (ref 1)
+in
+  case Lib.total Cond_rewr.mk_cond_rewrs (marker, b) of
+      NONE => die "EXN-FAILED!"
+    | SOME (rs as [(th',b')]) =>
+        if concl th' ~~ (marker |> concl |> strip_forall |> #2) then OK()
+        else die_r rs
+    | SOME rs => die_r rs
+end
 end (* local fun testb ... *)
 
 val _ = Process.exit Process.success