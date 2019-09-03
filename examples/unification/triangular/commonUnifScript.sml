HOL-Theorem-Prover/HOL

commit 7552446f03dfa69235c8135915468a4299a2dacb

Keep line lengths <= 80 columns in 2 files chosen arbitrarily

examples/unification/triangular/commonUnifScript.sml
@@ -1,4 +1,5 @@
-open HolKernel boolLib bossLib stringTheory arithmeticTheory pred_setTheory finite_mapTheory prim_recTheory
+open HolKernel boolLib bossLib stringTheory arithmeticTheory pred_setTheory
+     finite_mapTheory prim_recTheory
 val _ = new_theory "commonUnif";
 
 val _ = Hol_datatype `
@@ -7,27 +8,30 @@ val _ = Hol_datatype `
         | Num of num
         | String of string`;
 
-val extension_chain = Q.store_thm(
-"extension_chain",
-`!subst source z.
-  (((FINITE (source z)) /\
+Theorem extension_chain:
+  !subst source z.
+    FINITE (source z) /\
     (!m n. m < n ==> (subst m) SUBMAP (subst n)) /\
     (!n. DISJOINT (FDOM (subst n)) (source n)) /\
-    (!n. z < n ==> (FDOM (subst n) UNION (source n) = FDOM (subst z) UNION (source z))))
-    ==> ?x. !n. x < n ==> ((subst n) = (subst x)))`,
+    (!n. z < n ==>
+         FDOM (subst n) UNION (source n) = FDOM (subst z) UNION (source z))
+   ==>
+    ?x. !n. x < n ==> subst n = subst x
+Proof
 SPOSE_NOT_THEN STRIP_ASSUME_TAC THEN
-Q_TAC SUFF_TAC
 `!m. z <= m ==> ?n.m < n /\ CARD(source n) < CARD(source m)`
-THEN1
-(DISCH_THEN
- (STRIP_ASSUME_TAC o SIMP_RULE (srw_ss()) [SKOLEM_THM,GSYM RIGHT_EXISTS_IMP_THM]) THEN
-`?ff.!n.ff (SUC n) < ff n`
-    by (Q.EXISTS_TAC `\n.CARD(source(FUNPOW f n z))` THEN
-        SRW_TAC [][FUNPOW_SUC] THEN
-        Q_TAC SUFF_TAC `z <= FUNPOW f n z` THEN1 METIS_TAC [] THEN
-        Induct_on `n` THEN SRW_TAC [][FUNPOW_SUC] THEN
-        RES_TAC THEN DECIDE_TAC) THEN
-METIS_TAC [WF_LESS,WF_IFF_WELLFOUNDED,wellfounded_def]) THEN
+   suffices_by (
+    DISCH_THEN
+      (STRIP_ASSUME_TAC o
+       SIMP_RULE (srw_ss()) [SKOLEM_THM,GSYM RIGHT_EXISTS_IMP_THM]) THEN
+    `?ff.!n.ff (SUC n) < ff n`
+      by (Q.EXISTS_TAC `\n.CARD(source(FUNPOW f n z))` THEN
+          SRW_TAC [][FUNPOW_SUC] THEN
+          Q_TAC SUFF_TAC `z <= FUNPOW f n z` THEN1 METIS_TAC [] THEN
+          Induct_on `n` THEN SRW_TAC [][FUNPOW_SUC] THEN
+          RES_TAC THEN DECIDE_TAC) THEN
+    METIS_TAC [WF_LESS,WF_IFF_WELLFOUNDED,wellfounded_def]
+   ) THEN
 REPEAT STRIP_TAC THEN
 `?n.m < n /\ subst m <> subst n` by METIS_TAC [] THEN
 Q.EXISTS_TAC `n` THEN
@@ -36,12 +40,15 @@ Q.EXISTS_TAC `n` THEN
  FINITE (source m) /\ FINITE (source n)`
    by METIS_TAC [LESS_OR_EQ,FINITE_UNION,FDOM_FINITE] THEN
 `subst m SUBMAP subst n` by METIS_TAC [] THEN
-`FDOM (subst m) PSUBSET FDOM (subst n)` by METIS_TAC [SUBMAP_DEF,SUBSET_DEF,PSUBSET_DEF,EQ_FDOM_SUBMAP] THEN
-`(CARD (FDOM (subst n) UNION source n) = CARD (FDOM (subst n)) + CARD (source n)) /\
- (CARD (FDOM (subst m) UNION source m) = CARD (FDOM (subst m)) + CARD (source m)) /\
- (CARD (FDOM (subst m) UNION source m) = CARD (FDOM (subst n) UNION source n))` by
-  METIS_TAC [CARD_UNION,ADD_0,DISJOINT_DEF,CARD_EMPTY,FDOM_FINITE] THEN
-`CARD (FDOM (subst m)) < CARD (FDOM (subst n))` by METIS_TAC [CARD_PSUBSET,FDOM_FINITE]
-THEN DECIDE_TAC)
+`FDOM (subst m) PSUBSET FDOM (subst n)`
+   by METIS_TAC [SUBMAP_DEF,SUBSET_DEF,PSUBSET_DEF,EQ_FDOM_SUBMAP] THEN
+`CARD (FDOM(subst n) UNION source n) = CARD (FDOM(subst n)) + CARD (source n) /\
+ CARD (FDOM(subst m) UNION source m) = CARD (FDOM(subst m)) + CARD (source m) /\
+ CARD (FDOM(subst m) UNION source m) = CARD (FDOM(subst n) UNION source n))`
+   by METIS_TAC [CARD_UNION,ADD_0,DISJOINT_DEF,CARD_EMPTY,FDOM_FINITE] THEN
+`CARD (FDOM (subst m)) < CARD (FDOM (subst n))`
+   by METIS_TAC [CARD_PSUBSET,FDOM_FINITE]
+THEN DECIDE_TAC
+QED
 
 val _ = export_theory ();

examples/unification/triangular/ramanaLib.sml
@@ -6,7 +6,8 @@ open HolKernel boolLib bossLib Parse lcsymtacs pairLib
 val ERR = mk_HOL_ERR "ramanaLib"
 
 fun store_term_thm (s,t) =
-  store_thm(s,mk_comb(inst[``:'a``|->type_of t]``K:bool-> 'a -> bool T``,t),SIMP_TAC std_ss [])
+  store_thm(s,mk_comb(inst[``:'a``|->type_of t]``K:bool-> 'a -> bool T``,t),
+            SIMP_TAC std_ss [])
 
 fun store_type_thm (s,t) =
   store_thm(s,inst[``:'a``|->t]``K T (x:'a)``,SIMP_TAC std_ss [])
@@ -34,7 +35,8 @@ fun RWDefine q =
   case q |> Absyn |> Defn.parse_absyn of
     (_,[name]) => Define q before export_rewrites[name^"_def"]
   | _ => raise ERR "RWDefine" "Multiple definitions"
-fun RWnew_specification (s,l,t) = new_specification (s,l,t) before export_rewrites [s]
+fun RWnew_specification (s,l,t) =
+    new_specification (s,l,t) before export_rewrites [s]
 fun RWtDefine s q t = tDefine s q t before export_rewrites [s^"_def"]
 
 val CONJ1_TAC = conj_asm1_tac

commit 448562b256339a8beb0e0cacae4fbab344677a64

Fix commonUnifScript, given breakage introduced by 7552446f

examples/unification/triangular/commonUnifScript.sml
@@ -42,9 +42,9 @@ Q.EXISTS_TAC `n` THEN
 `subst m SUBMAP subst n` by METIS_TAC [] THEN
 `FDOM (subst m) PSUBSET FDOM (subst n)`
    by METIS_TAC [SUBMAP_DEF,SUBSET_DEF,PSUBSET_DEF,EQ_FDOM_SUBMAP] THEN
-`CARD (FDOM(subst n) UNION source n) = CARD (FDOM(subst n)) + CARD (source n) /\
- CARD (FDOM(subst m) UNION source m) = CARD (FDOM(subst m)) + CARD (source m) /\
- CARD (FDOM(subst m) UNION source m) = CARD (FDOM(subst n) UNION source n))`
+`CARD (FDOM(subst n) UNION source n) = CARD (FDOM(subst n)) + CARD (source n) ∧
+ CARD (FDOM(subst m) UNION source m) = CARD (FDOM(subst m)) + CARD (source m) ∧
+ CARD (FDOM(subst m) UNION source m) = CARD (FDOM(subst n) UNION source n)`
    by METIS_TAC [CARD_UNION,ADD_0,DISJOINT_DEF,CARD_EMPTY,FDOM_FINITE] THEN
 `CARD (FDOM (subst m)) < CARD (FDOM (subst n))`
    by METIS_TAC [CARD_PSUBSET,FDOM_FINITE]