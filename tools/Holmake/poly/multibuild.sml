HOL-Theorem-Prover/HOL

commit e7220bcf5c344c8486d2222dd0129e8f8ddc2793

Make tags prettier again

tools/Holmake/poly/multibuild.sml
@@ -85,6 +85,8 @@ fun graphbuild optinfo g =
             val _ = is_pending (#status nI) orelse
                     raise Fail "runnable not pending"
             val target_s = dep_toString (#target nI)
+            val tag = if OS.Path.dir target_s = dir then OS.Path.file target_s
+                      else target_s
             fun stdprocess() =
               case #command nI of
                   NoCmd => genjob (updnode (n,Succeeded) g, true)
@@ -126,7 +128,7 @@ fun graphbuild optinfo g =
                                 (g',ok')
                               end
                         in
-                          NewJob ({tag = target_s, command = shell_command c,
+                          NewJob ({tag = tag, command = shell_command c,
                                    update = update, dir = dir},
                                   (updall(g, Running), true))
                         end
@@ -159,7 +161,7 @@ fun graphbuild optinfo g =
                             diag ("Other nodes are: "^
                                   String.concatWith ", "
                                         (map node_toString other_nodes));
-                            NewJob({tag = target_s, dir = dir,
+                            NewJob({tag = tag, dir = dir,
                                     command = cline_to_command cline,
                                     update = update},
                                    (updall Running g, true))

commit 0b43297e9583e3d7d3469e7dbc7d3dbf804f882d

Fix regression failure caused by e7220bcf5c34

tools/Holmake/poly/multibuild.sml
@@ -43,8 +43,7 @@ fun graphbuild optinfo g =
         then String.substring(s,0,String.size s - 4)
         else s
     fun safetag d t =
-        if d = OS.Path.dir t then dropthySuffix (OS.Path.file t)
-        else String.map (fn #"/" => #"-" | c => c) t
+        String.map (fn #"/" => #"-" | c => c) (dropthySuffix t)
     fun genLF {tag, dir} =
         let
           val ldir = dir ++ loggingdir