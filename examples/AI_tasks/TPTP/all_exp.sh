76a9923b08c37887d3c02781366989cee047de64
Thibault Gauthier <email@thibaultgauthier.fr>
2020-02-16 15:36:23 +0100

    script performing all experiments

---
+++ examples/AI_tasks/TPTP/all_exp.sh
@@ -0,0 +1,6 @@
+sh parallel_eprover.sh combin_test
+sh parallel_vampire.sh combin_test
+sh parallel_z3.sh combin_test
+sh parallel_eprover.sh combin_train
+sh parallel_vampire.sh combin_train
+sh parallel_z3.sh combin_train
