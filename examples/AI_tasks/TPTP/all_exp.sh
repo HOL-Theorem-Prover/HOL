27627fb403289797fa92446b4f9610b2f48559f5
Thibault Gauthier <email@thibaultgauthier.fr>
2020-02-16 15:38:33 +0100

    number of cores

--- examples/AI_tasks/TPTP/all_exp.sh
+++ examples/AI_tasks/TPTP/all_exp.sh
@@ -1,6 +1,6 @@
-sh parallel_eprover.sh combin_test
-sh parallel_vampire.sh combin_test
-sh parallel_z3.sh combin_test
-sh parallel_eprover.sh combin_train
-sh parallel_vampire.sh combin_train
-sh parallel_z3.sh combin_train
+sh parallel_eprover.sh 40 combin_test
+sh parallel_vampire.sh 40 combin_test
+sh parallel_z3.sh 40 combin_test
+sh parallel_eprover.sh 40 combin_train
+sh parallel_vampire.sh 40 combin_train
+sh parallel_z3.sh 40 combin_train
