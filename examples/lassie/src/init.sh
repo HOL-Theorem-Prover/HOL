860438870efdbdfa8cb4074923b472ffd8b3189e
Heiko Becker <hbecker@mpi-sws.org>
2021-01-14 11:38:19 +0100

    Possibly fix the compilation bug for lassie

--- examples/lassie/src/init.sh
+++ examples/lassie/src/init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ $LASSIEDIR == "" ]; then
-  export LASSIEDIR = ./ #recovery attempt
+if [ "$LASSIEDIR" == "" ]; then
+  export LASSIEDIR=../ #recovery attempt
 fi
 
 if [[ -d "$LASSIEDIR/sempre/fig" ]]
 then
   echo "SEMPRE already initialized skipping intialization"
 else
-  cd $LASSIEDIR/sempre &&\
-  ./pull-dependencies core interactive &&\
-  ant core interactive &&\
+  cd $LASSIEDIR/sempre 
+  echo "Initialization running" 
+  ./pull-dependencies core interactive
+  #ant core interactive
+  echo "Initialization done"
   cd $LASSIEDIR/src
 fi
