f2bcb6b5e821e6586eb9973c87dbcb85912b9298
Henrik Karlsson <henrik10@kth.se>
2021-05-19 12:35:58 +0200

    Added if-else macro on getUInt for compatibility.

--- src/HolSat/sat_solvers/minisat/File.C
+++ src/HolSat/sat_solvers/minisat/File.C
@@ -118,8 +118,11 @@ void putUInt(File& out, uint64 val)
 }
 
 
-uint64 getUInt(File& in)
-    noexcept(false)
+#if __cplusplus >= 201703L
+uint64 getUInt (File& in) noexcept(false)
+#else
+uint64 getUInt (File& in) throw(Exception_EOF)
+#endif
 {
     uint byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7;
     byte0 = in.getChar();
