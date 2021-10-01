HOL-Theorem-Prover/HOL

commit c4d55fc2a6198d1fb2c3eb43bdd6790cae807a30

Rename bagTheory's MEMBER_NOT_EMPTY to avoid name-clash with pred_set

src/bag/bagScript.sml
@@ -557,10 +557,11 @@ val BAG_INN_EMPTY_BAG = Q.store_thm(
   SIMP_TAC arith_ss [BAG_INN, EMPTY_BAG, EQ_IMP_THM])
 val _ = export_rewrites ["BAG_INN_EMPTY_BAG"];
 
-val MEMBER_NOT_EMPTY = store_thm(
-  "MEMBER_NOT_EMPTY",
-  ``!b. (?x. BAG_IN x b) = ~(b = EMPTY_BAG)``,
-  METIS_TAC [BAG_cases, BAG_IN_BAG_INSERT, NOT_IN_EMPTY_BAG]);
+Theorem BAG_MEMBER_NOT_EMPTY:
+  !b. (?x. BAG_IN x b) <=> b <> EMPTY_BAG
+Proof
+  METIS_TAC [BAG_cases, BAG_IN_BAG_INSERT, NOT_IN_EMPTY_BAG]
+QED
 
 val _ = print "Properties of SUB_BAG\n"
 

commit cf32220437a948e43d98b65374ff189830d0baf2

Fix breakage caused by c4d55fc2a6198

src/bag/bagScript.sml
@@ -1697,7 +1697,7 @@ Proof
       HO_MATCH_MP_TAC STRONG_FINITE_BAG_INDUCT >>
       rw[]
         >- (Cases_on `s={}` >- rw[] >>
-            `?e. e IN s` by simp[pred_setTheory.MEMBER_NOT_EMPTY] >>
+            `?e. e IN s` by simp[MEMBER_NOT_EMPTY] >>
             fs[BAG_OF_SET,EMPTY_BAG_alt,FUN_EQ_THM] >>
             first_x_assum (qspec_then `e` mp_tac) >>
             rw[])
@@ -1953,7 +1953,7 @@ QED
 val BAG_CHOICE_DEF = new_specification
   ("BAG_CHOICE_DEF",["BAG_CHOICE"],
    Q.prove(`?ch:('a -> num) -> 'a. !b. ~(b = {||}) ==> BAG_IN (ch b) b`,
-           PROVE_TAC [MEMBER_NOT_EMPTY]));
+           PROVE_TAC [BAG_MEMBER_NOT_EMPTY]));
 
 
 (* ===================================================================== *)