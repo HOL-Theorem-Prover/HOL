3e6f7e8b0f0fd0b8e86a8fbffe82d82ea35a1b5b
Chun Tian (binghe) <binghe.lisp@gmail.com>
2021-10-25 13:24:58 +0200

    Upgraded llist.thy to hol4-coalgebras.thy

---
+++ src/coalgebras/hol4-coalgebras-unint.thy
@@ -0,0 +1,26 @@
+name: hol-coalgebras
+version: 1.0
+description: HOL coalgebras theories (before re-interpretation)
+author: HOL OpenTheory Packager <opentheory-packager@hol-theorem-prover.org>
+license: GPL
+main {
+  import: llist
+  import: ltree
+  import: lbtree
+  import: path
+}
+llist {
+  article: "llist.ot.art"
+}
+ltree {
+  import: llist
+  article: "ltree.ot.art"
+}
+lbtree {
+  import: llist
+  article: "lbtree.ot.art"
+}
+path {
+  import: llist
+  article: "path.ot.art"
+}
