6a49dd8ad0ae4dfbabc7d81c2c1d8ba032d655f2
Chun Tian (binghe) <binghe.lisp@gmail.com>
2022-07-05 23:59:33 +0200

    Now include "realax" as part of "hol-real" OT package

--- src/real/hol4-real-unint.thy
+++ src/real/hol4-real-unint.thy
@@ -4,6 +4,7 @@ description: HOL real theories (before re-interpretation)
 author: HOL OpenTheory Packager <opentheory-packager@hol-theorem-prover.org>
 license: MIT
 main {
+  import: realax
   import: real
   import: intreal
   import: iterate
@@ -11,27 +12,36 @@ main {
   import: metric
   import: nets
 }
+realax {
+  article: "realax.ot.art"
+}
 real {
+  import: realax
   article: "real.ot.art"
 }
 intreal {
+  import: realax
   import: real
   article: "intreal.ot.art"
 }
 iterate {
+  import: realax
   import: real
   article: "iterate.ot.art"
 }
 real-sigma {
+  import: realax
   import: real
   import: iterate
   article: "real_sigma.ot.art"
 }
 metric {
+  import: realax
   import: real
   article: "metric.ot.art"
 }
 nets {
+  import: realax
   import: real
   import: metric
   article: "nets.ot.art"
