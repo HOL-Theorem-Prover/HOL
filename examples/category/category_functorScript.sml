HOL-Theorem-Prover/HOL

commit 697c46653e224585f05ee616c270aef83920425e

Work on generalising tech behind f⦇ k1 ↦ v1; k2 ↦ v2⦈ syntax
Idea is to have this available for lists, with l❲i ↦ e1; j ↦ e2❳
encoding nested LUPDATE terms, while still also retaining l❲i❳ for
indexing with EL.

Parsing appears to work, but pretty-printing is not yet handled
generically.

src/combin/combinpp.sig
@@ -1,15 +1,16 @@
 signature combinpp =
 sig
 
-val toplevel_updname : string
-val internal_consupd : string
-val internal_idupd : string
-val mapsto_special : string
-val update_constname : string
-
 val upd_processor : term_grammar.absyn_postprocessor
 val upd_printer : term_grammar.userprinter
 
+val new_form :
+    { left : string, right : string, upd_term_name : string,
+      lookup_term_name : string option } ->
+    unit
+
+val enable_dictsyntax : unit -> unit
+  (* only needs calling once *)
 end
 
 (*

src/combin/combinpp.sml
@@ -1,76 +1,147 @@
 structure combinpp :> combinpp =
 struct
 
-open HolKernel boolLib Absyn
-val toplevel_updname = "  combinpp.toplevelupd"
+open HolKernel boolLib Parse Absyn
 val internal_consupd = "  combinpp.cons"
 val internal_idupd = "  combinpp.nil"
 val mapsto_special = "  combinpp.leftarrow"
-val update_constname = "combinpp.UPDATE"
+val toplevel_updname = "  combinpp.top"
+
+datatype dict_delta = DD of { left : string, right : string,
+                              upd_term_name : string,
+                              lookup_term_name : string option }
+
+local open ThyDataSexp
+in
+val (enc,dec) = bij_ed (
+      (fn DD {left=l,right=r,upd_term_name=utn,lookup_term_name=ltn} =>
+          (l,r,utn,ltn)),
+      (fn (l,r,utn,ltn) => DD {left = l, right = r, upd_term_name = utn,
+                               lookup_term_name = ltn})
+    ) (
+      pair4_ed (
+        string_ed, string_ed, string_ed, option_ed string_ed
+      )
+    )
+end
+
+type pppdb = {parse: dict_delta Symtab.table,
+              print_upd: dict_delta Symtab.table,
+              print_lookup: dict_delta Symtab.table}
+val empty_pppdb : pppdb = {parse = Symtab.empty, print_upd = Symtab.empty,
+                           print_lookup = Symtab.empty}
+
+fun pppdb_apply (dd as DD{left,right,upd_term_name=utn,lookup_term_name=ltn})
+                ({parse,print_upd,print_lookup} : pppdb) : pppdb =
+    {parse = Symtab.update(left,dd) parse,
+     print_upd = Symtab.update(utn,dd) print_upd,
+     print_lookup = case ltn of NONE => print_lookup
+                              | SOME s => Symtab.update(s,dd) print_lookup}
+val pppdata_info = {
+  tag = "dictppp", initial_values = [("min", empty_pppdb)],
+  apply_delta = pppdb_apply
+}
+val {DB, record_delta, get_global_value, ...} = AncestryData.fullmake {
+      adinfo = pppdata_info,
+      uptodate_delta = fn _ => true,
+      sexps = {dec = dec, enc = enc},
+      globinfo = {apply_to_global = pppdb_apply, thy_finaliser = NONE,
+                  initial_value = empty_pppdb}
+    }
 
 fun ERR f l msg = Feedback.mk_HOL_ERRloc "combinpp" f l msg
 fun cAPP l (a1, a2) = Absyn.APP(l,a1,a2)
 
-fun break_binop ident a =
+fun gen_break_binop P f a =
     case a of
         APP(l, a1, a2) =>
           (case a1 of
                APP(l1, IDENT (l1', s), a12) =>
-               if s = ident then SOME (l, a12, a2)
+               if P s then SOME ((l, a12, a2), f s)
                else NONE
              | _ => NONE)
       | _ => NONE
 
+fun break_binop ident a =
+    Option.map #1 (gen_break_binop (fn s => s = ident) (fn s => s) a)
+
 fun break_arrow a =
     case break_binop mapsto_special a of
-        SOME(l, a1, a2) => (a1,a2)
-      | NONE => raise ERR "upd_processor" (locn_of_absyn a)
-                      "Expected to find maps-to form"
+        SOME(l, a1, a2) => SOME (a1,a2)
+      | NONE => NONE
 
-fun process_updates f upds =
-    case upds of
-        IDENT (l, inner) => if inner = internal_idupd then f
-                            else raise ERR "upd_processor" l
-                                       "Fn-update syntax bottoms out badly"
-      | _ =>
-          (case break_binop internal_consupd upds of
-               SOME (l, arg1, arg2) =>
-               let
-                 val (d,r) = break_arrow arg1
-                 val t = process_updates f arg2
-               in
-                 cAPP l (
-                   cAPP (locn.between (locn_of_absyn d) (locn_of_absyn r)) (
-                     cAPP (locn_of_absyn d) (
-                       IDENT (locn.Loc_None, update_constname),
-                       d
-                     ),
-                     r
-                   ),
-                   t
-                 )
-               end
-             | NONE => raise ERR "upd_processor" (locn_of_absyn upds)
-                             "Fn-update syntax update broken")
+fun islistnil a_t =
+    case a_t of
+        IDENT (l, nm) => nm = internal_idupd
+      | _ => false
 
-fun upd_processor G a =
+fun process_updates (update_name, lookup_name_opt) f upds =
+    if islistnil upds then f
+    else
+      case break_binop internal_consupd upds of
+          SOME (l, arg1, arg2) =>
+          let
+          in
+            case break_arrow arg1 of
+                SOME (d,r) =>
+                let
+                  val t = process_updates (update_name, lookup_name_opt) f arg2
+                in
+                  cAPP l (
+                    cAPP (locn.between(locn_of_absyn d)(locn_of_absyn r)) (
+                      cAPP (locn_of_absyn d) (
+                        IDENT (locn.Loc_None, update_name),
+                        d
+                      ),
+                      r
+                    ),
+                    t
+                  )
+                end
+              | NONE =>
+                if islistnil arg2 then
+                  case lookup_name_opt of
+                      NONE => raise ERR "upd_processor" (locn_of_absyn upds)
+                                    "Malformed update argument"
+                    | SOME ln =>
+                      cAPP l (cAPP l (IDENT (locn.Loc_None, ln), arg1), f)
+                else
+                  raise ERR "upd_processor" (locn_of_absyn arg1)
+                        "Update list element should be update but isn't"
+          end
+        | NONE => raise ERR "upd_processor" (locn_of_absyn upds)
+                        "Fn-update syntax update broken"
+
+fun upd_processor0 (DB:pppdb) a =
     case a of
-        LAM (l,v,a0) => LAM (l,v,upd_processor G a0)
-      | TYPED (l, a0, pty) => TYPED(l, upd_processor G a0, pty)
+        LAM (l,v,a0) => LAM (l,v,upd_processor0 DB a0)
+      | TYPED (l, a0, pty) => TYPED(l, upd_processor0 DB a0, pty)
       | APP(l,a1,a2) =>
-        (case break_binop toplevel_updname a of
-             NONE => APP(l, upd_processor G a1, upd_processor G a2)
-           | SOME (_, arg10, arg20) =>
+        (case
+            gen_break_binop
+              (fn s => String.isPrefix toplevel_updname s)
+              (fn s => String.extract(s, size toplevel_updname, NONE))
+              a
+           of
+             NONE => APP(l, upd_processor0 DB a1, upd_processor0 DB a2)
+           | SOME ((_, arg10, arg20), left) =>
              let
-               val arg1 = upd_processor G arg10
-               val arg2 = upd_processor G arg20
+               val arg1 = upd_processor0 DB arg10
+               val arg2 = upd_processor0 DB arg20
              in
-               process_updates arg1 arg2
+               case Symtab.lookup(#parse DB) left of
+                   NONE => raise ERR "upd_processor" (locn_of_absyn a)
+                                 ("No stored info for " ^ left)
+                 | SOME (DD {lookup_term_name, upd_term_name, ...}) =>
+                   process_updates (upd_term_name, lookup_term_name) arg1 arg2
              end)
-      | _ => a;
+      | _ => a
+
+fun upd_processor G a = upd_processor0 (get_global_value()) a
 
 val _ = term_grammar.userSyntaxFns.register_absynPostProcessor
-          {name = "combin.UPDATE", code = upd_processor}
+          {name = "combin.UPDATE",
+           code = upd_processor}
 
 fun has_name_by_parser G s tm = let
   open GrammarSpecials
@@ -147,5 +218,37 @@ fun upd_printer (tyg,tmg) backend printer ppfns (pgr,lgr,rgr) depth tm =
 
 val _ = term_grammar.userSyntaxFns.register_userPP
           {name = "combin.updpp", code = upd_printer}
+
+fun enable_dictsyntax () = (
+      set_mapped_fixity {fixity = Infix(NONASSOC,100),
+                         term_name = mapsto_special,
+                         tok = "|->"};
+      set_mapped_fixity {fixity = Infix(NONASSOC,100),
+                         term_name = mapsto_special,
+                         tok = "↦"};
+      add_absyn_postprocessor "combin.UPDATE"
+)
+
+fun addlform l r =
+    add_rule {block_style = (AroundEachPhrase, (PP.CONSISTENT, 0)),
+              fixity = Suffix 2100,
+              paren_style = OnlyIfNecessary,
+              pp_elements = [
+                TOK l,
+                ListForm {
+                  separator = [TOK ";", BreakSpace(1,0)],
+                  block_info = (PP.CONSISTENT, 1),
+                  cons = internal_consupd,
+                  nilstr = internal_idupd
+                },
+                TOK r],
+              term_name = toplevel_updname^l};
+
+
+
+fun new_form (r as {left,right,upd_term_name,lookup_term_name}) = (
+  addlform left right;
+  record_delta (DD r)
+)
 
 end (* struct *)

src/combin/combinScript.sml
@@ -39,36 +39,25 @@ val _ = Parse.Unicode.unicode_version {tmnm = "o", u = UTF8.chr 0x2218}
 val _ = TeX_notation {hol = "o", TeX = ("\\HOLTokenCompose", 1)}
 val _ = TeX_notation {hol = UTF8.chr 0x2218, TeX = ("\\HOLTokenCompose", 1)}
 
-val _ = let
-  open combinpp
-  fun addlform l r =
-      add_rule {block_style = (AroundEachPhrase, (PP.CONSISTENT, 0)),
-                fixity = Suffix 2100,
-                paren_style = OnlyIfNecessary,
-                pp_elements = [
-                  TOK l,
-                  ListForm {
-                    separator = [TOK ";", BreakSpace(1,0)],
-                    block_info = (PP.CONSISTENT, 1),
-                    cons = internal_consupd,
-                    nilstr = internal_idupd
-                  },
-                  TOK r],
-                term_name = toplevel_updname};
-in
-  set_mapped_fixity {fixity = Infix(NONASSOC,100),
-                     term_name = mapsto_special,
-                     tok = "|->"};
-  set_mapped_fixity {fixity = Infix(NONASSOC,100),
-                     term_name = mapsto_special,
-                     tok = "↦"};
-  addlform "(|" "|)";
-  addlform UnicodeChars.lensel UnicodeChars.lenser;
-  add_ML_dependency "combinpp";
-  add_absyn_postprocessor "combin.UPDATE";
-  inferior_overload_on (update_constname, ``UPDATE``);
-  add_user_printer ("combin.updpp", “UPDATE k v f”)
-end;
+
+val _ = add_ML_dependency "combinpp";
+val _ = combinpp.enable_dictsyntax()
+
+val _ = combinpp.new_form {
+  left = "(|", right = "|)", upd_term_name = "UPDATE",
+  lookup_term_name = NONE
+  }
+
+val _ = combinpp.new_form {
+  left = UnicodeChars.lensel, right = UnicodeChars.lenser,
+  upd_term_name = "UPDATE",
+  lookup_term_name = NONE
+  }
+
+val _ = add_user_printer ("combin.updpp", “UPDATE k v f”)
+
+
+
 
 val _ = TeX_notation {TeX = ("\\llparenthesis", 1), hol = UnicodeChars.lensel}
 val _ = TeX_notation {TeX = ("\\llparenthesis", 1), hol = "(|"}

src/list/src/listScript.sml
@@ -236,14 +236,10 @@ Proof
 QED
 
 (* array subscript style brackets are U+2772 and U+2773 *)
-val _ = add_rule {
-  block_style = (AroundEachPhrase, (PP.CONSISTENT, 0)),
-  fixity = Suffix 2100, paren_style = OnlyIfNecessary,
-  pp_elements = [TOK "❲", BreakSpace(0,2),TM, BreakSpace(0,0), TOK "❳"],
-  term_name = "flipEL"
-};
-Overload flipEL = “λl n. EL n l”
-
+val _ = combinpp.new_form {
+  left = "❲", right = "❳",
+  upd_term_name = "fLUPDATE", lookup_term_name = SOME "EL"
+  }
 val _ = TeX_notation { hol = "❲", TeX = ("\\HOLTokenLeftELbracket{}", 1)}
 val _ = TeX_notation { hol = "❳", TeX = ("\\HOLTokenRightELbracket{}", 1)}
 
@@ -3848,6 +3844,8 @@ Theorem LUPDATE_def[userdef]:
 Proof
   simp[LUPDATE_DEF]
 QED
+
+Overload fLUPDATE = “λk v l. LUPDATE v k l”
 
 val _ = DefnBase.register_indn $ Prim_rec.gen_indthm
            {lookup_ind = TypeBase.induction_of} LUPDATE_DEF

commit 7ae9040ac69e475d38b1c64f1eca150c44b24c99

Provide method to remove general point-update syntax
Use this in examples/category/category_functor, fixing build error
introduced by 697c46653e.

examples/category/category_functorScript.sml
@@ -4,7 +4,7 @@ Ancestors
 
 val _ = ParseExtras.temp_loose_equality()
 
-val _ = remove_termtok { term_name = combinpp.toplevel_updname, tok = "(|" }
+val _ = combinpp.remove_paren_syntax "(|"
 
 val _ = type_abbrev_pp("functor",
 ``:((α,β) category, (γ,δ) category, (α,β) mor -> (γ,δ) mor)

src/combin/combinpp.sig
@@ -9,6 +9,8 @@ val new_form :
       lookup_term_name : string option } ->
     unit
 
+val remove_paren_syntax : string -> unit
+
 val enable_dictsyntax : unit -> unit
   (* only needs calling once *)
 end

src/combin/combinpp.sml
@@ -10,10 +10,11 @@ val toplevel_updname = "  combinpp.top"
 datatype dict_delta = DD of { left : string, right : string,
                               upd_term_name : string,
                               lookup_term_name : string option }
+datatype delta = ADD of dict_delta | RM of string
 
 local open ThyDataSexp
 in
-val (enc,dec) = bij_ed (
+val dded as (ddenc,dddec) = bij_ed (
       (fn DD {left=l,right=r,upd_term_name=utn,lookup_term_name=ltn} =>
           (l,r,utn,ltn)),
       (fn (l,r,utn,ltn) => DD {left = l, right = r, upd_term_name = utn,
@@ -23,6 +24,10 @@ val (enc,dec) = bij_ed (
         string_ed, string_ed, string_ed, option_ed string_ed
       )
     )
+val (delta_enc,delta_dec) = bij_ed (
+      (fn ADD dd => inl dd | RM s => inr s),
+      (fn inl dd => ADD dd | inr s => RM s)
+    ) (tagged_sum ("add", dded) ("remove", string_ed))
 end
 
 type pppdb = {parse: dict_delta Symtab.table,
@@ -31,21 +36,36 @@ type pppdb = {parse: dict_delta Symtab.table,
 val empty_pppdb : pppdb = {parse = Symtab.empty, print_upd = Symtab.empty,
                            print_lookup = Symtab.empty}
 
-fun pppdb_apply (dd as DD{left,right,upd_term_name=utn,lookup_term_name=ltn})
-                ({parse,print_upd,print_lookup} : pppdb) : pppdb =
+fun pppdb_apply_add
+      (dd as DD{left,right,upd_term_name=utn,lookup_term_name=ltn})
+      ({parse,print_upd,print_lookup} : pppdb) : pppdb =
     {parse = Symtab.update(left,dd) parse,
      print_upd = Symtab.update(utn,dd) print_upd,
      print_lookup = case ltn of NONE => print_lookup
                               | SOME s => Symtab.update(s,dd) print_lookup}
+fun pppdb_apply_rm s {parse,print_upd,print_lookup} =
+    let
+      fun foldthis (p as (k, DD{left,...})) tab0 =
+          if left = s then tab0 else Symtab.update p tab0
+      fun rebuild tab0 = Symtab.fold foldthis tab0
+    in
+      {parse = Symtab.delete s parse handle Symtab.UNDEF _ => parse,
+       print_upd = Symtab.build (rebuild print_upd),
+       print_lookup = Symtab.build (rebuild print_lookup)}
+    end
+
+fun apply_delta (ADD dd) db = pppdb_apply_add dd db
+  | apply_delta (RM s) db = pppdb_apply_rm s db
+
 val pppdata_info = {
   tag = "dictppp", initial_values = [("min", empty_pppdb)],
-  apply_delta = pppdb_apply
+  apply_delta = apply_delta
 }
 val {DB, record_delta, get_global_value, ...} = AncestryData.fullmake {
       adinfo = pppdata_info,
       uptodate_delta = fn _ => true,
-      sexps = {dec = dec, enc = enc},
-      globinfo = {apply_to_global = pppdb_apply, thy_finaliser = NONE,
+      sexps = {dec = delta_dec, enc = delta_enc},
+      globinfo = {apply_to_global = apply_delta, thy_finaliser = NONE,
                   initial_value = empty_pppdb}
     }
 
@@ -248,7 +268,12 @@ fun addlform l r =
 
 fun new_form (r as {left,right,upd_term_name,lookup_term_name}) = (
   addlform left right;
-  record_delta (DD r)
+  record_delta (ADD (DD r))
+)
+
+fun remove_paren_syntax lparen_name = (
+  remove_termtok {tok = lparen_name, term_name = toplevel_updname ^ lparen_name};
+  record_delta (RM lparen_name)
 )
 
 end (* struct *)