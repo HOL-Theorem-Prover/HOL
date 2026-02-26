f5fb4840dd24e9feb5d515cee2d9f2cc6f536255
Ramana Kumar <ramana@member.fsf.org>
2026-02-26 15:15:28 +0000

    Add the shell script actually used for benchmarking

---
+++ src/postkernel/prooftrace/benchmarks.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+git checkout c38cd4913
+git clean -xdf
+poly --script tools/smart-configure.sml
+/usr/bin/time -v bin/build --nograph &| tee /tmp/bench-a.log
+git checkout cc2167173
+git clean -xdf
+poly --script tools/smart-configure.sml
+/usr/bin/time -v bin/build --nograph &| tee /tmp/bench-b.log
+git clean -xdf
+poly --script tools/smart-configure.sml
+/usr/bin/time -v bin/build --nograph --trace &| tee /tmp/bench-c.log
+cd examples/formal-languages/regular
+/usr/bin/time -v ../../../bin/Holmake --trace &| tee /tmp/bench-c-regexp.log
+cd -
+find . -name "*.pft" -o -name "*.pft.zst" | \
+  xargs du -ch | tail -1 | tee /tmp/bench-c-size.log
+/usr/bin/time -v bin/prooftrace merge -o bench.pft \
+  arithmetic.DIVISION \
+  regexp_compiler.regexp_matcher_correct \
+  &| tee /tmp/bench-d.log
+zstd bench.pft
+ls -lh bench.pft bench.pft.zst | tee -a /tmp/bench-d.log
+/usr/bin/time -v bin/prooftrace replay bench.pft \
+  &| tee /tmp/bench-e.log
